110220c48b7f40245d887ee2c947aae328a271c7
Toru Hisai <toru@torus.jp>
2021-04-02 18:56:36 +0900

    単純な文字列の読み込みを追加。

--- parse.c
+++ parse.c
@@ -42,23 +42,85 @@ void	parse_ungetc(t_parse_buffer *buf)
 
 typedef enum
 {
-	TOKTYPE_STRING,
-	TOKTYPE_VARIABLE,
+	TOKTYPE_NON_EXPANDABLE,
+	TOKTYPE_EXPANDABLE,
 	TOKTYPE_PIPE,
 	TOKTYPE_INPUT_REDIRECTION,
 	TOKTYPE_OUTPUT_REDIRECTION,
 	TOKTYPE_OUTPUT_APPENDING,
 	TOKTYPE_SEMICOLON,
+	TOKTYPE_NEWLINE,
+	TOKTYPE_SPACE,
 }	t_parse_token_type;
 
 typedef struct	s_parse_token
 {
-	char	text[PARSE_BUFFER_SIZE + 1];
+	char				text[PARSE_BUFFER_SIZE];
+    int					length;
 	t_parse_token_type	type;
 }	t_parse_token;
 
+int	parse_is_special_char(char ch)
+{
+    if (ch == ' ' || ch == '\t' || ch == '\n' || ch == ';'
+        || ch == '|' || ch == '>' || ch == '<')
+        return (1);
+    return (0);
+}
+
 int	parse_read_word(t_parse_buffer *buf, t_parse_token *result)
 {
+    int	pos;
+    int	ch;
+
+    pos = 0;
+    while (1)
+    {
+        ch = parse_getc(buf);
+        if (ch == EOF)
+            break ;
+        if (parse_is_special_char(ch))
+        {
+            parse_ungetc(buf);
+            break ;
+        }
+        result->text[pos++] = ch;
+    }
+    result->length = pos;
+    return (1);
+}
+
+int	parse_read_double_quoted(t_parse_buffer *buf, t_parse_token *result)
+{
+    int	pos;
+    int	ch;
+
+    pos = 0;
+    while (1)
+    {
+        ch = parse_getc(buf);
+        if (ch == '"' || ch == '\n' || ch == EOF)
+            break ;
+        result->text[pos++] = ch;
+    }
+    result->length = pos;
+    return (1);
+}
+
+int	parse_read_single_quoted(t_parse_buffer *buf, t_parse_token *result)
+{
+    int	pos;
+    int	ch;
+
+    pos = 0;
+    while (1)
+    {
+        ch = parse_getc(buf);
+        if (ch == '\'' || ch == '\n' || ch == EOF)
+            break ;
+        result->text[pos++] = ch;
+    }
+    result->length = pos;
     return (1);
 }
 
@@ -66,29 +128,68 @@ int parse_get_token(t_parse_buffer *buf, t_parse_token *result)
 {
 	int				ch;
 
-    while (1)
+    ch = parse_getc(buf);
+    if (ch == ' ' || ch == '\t')
     {
-        ch = parse_getc(buf);
-        if (ch != ' ' || ch != '\n' || ch != EOF)
-            break ;
+        while (1)
+        {
+            ch = parse_getc(buf);
+            if (ch != ' ' && ch != '\t')
+            {
+                if (ch != EOF)
+                    parse_ungetc(buf);
+                result->type = TOKTYPE_SPACE;
+                break ;
+            }
+        }
     }
-	if (ch == '$')
+    else if (ch == '\n')
 	{
-        return (parse_read_word(buf, result));
+        result->type = TOKTYPE_NEWLINE;
+        return (1);
 	}
 	else if (ch == ';')
 	{
+        result->type = TOKTYPE_SEMICOLON;
+        return (1);
+	}
+	else if (ch == '|')
+	{
+        result->type = TOKTYPE_PIPE;
+        return (1);
 	}
 	else if (ch == '<')
 	{
+        result->type = TOKTYPE_INPUT_REDIRECTION;
+        return (1);
 	}
 	else if (ch == '>')
 	{
+        ch = parse_getc(buf);
+        if (ch == '>')
+            result->type = TOKTYPE_OUTPUT_APPENDING;
+        else
+        {
+            result->type = TOKTYPE_OUTPUT_REDIRECTION;
+            if (ch != EOF)
+                parse_ungetc(buf);
+        }
+        return (1);
 	}
 	else if (ch == '"')
 	{
+        result->type = TOKTYPE_EXPANDABLE;
+        return (parse_read_double_quoted(buf, result));
 	}
 	else if (ch == '\'')
 	{
+        result->type = TOKTYPE_NON_EXPANDABLE;
+        return (parse_read_single_quoted(buf, result));
 	}
+    else if (ch != EOF)
+    {
+        result->type = TOKTYPE_EXPANDABLE;
+        parse_ungetc(buf);
+        return (parse_read_word(buf, result));
+    }
 }
--- test/parse_test.c
+++ test/parse_test.c
@@ -6,7 +6,7 @@
 /*   By: thisai <thisai@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/02 11:27:21 by thisai            #+#    #+#             */
-/*   Updated: 2021/04/02 11:53:59 by thisai           ###   ########.fr       */
+/*   Updated: 2021/04/02 12:15:58 by thisai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,17 +39,48 @@ void	print_result()
 
 #define CHECK(val) check((int64_t)val, #val)
 #define CHECK_EQ(actual, expected) check(actual == expected, #actual " == " #expected)
+#define TEST_SECTION(message) printf("- " message "\n")
+
+void	init_buf_with_string(t_parse_buffer *buf, const char* str)
+{
+    buf->cur_pos = 0;
+    strcpy(buf->buffer, str);
+    buf->size = strlen(str);
+}
 
 int main()
 {
-	t_parse_buffer buf;
-	buf.cur_pos = 0;
+    TEST_SECTION("parse_getc");
+    {
+        t_parse_buffer buf;
+        init_buf_with_string(&buf, "abcde");
+        int x = parse_getc(&buf);
+        CHECK_EQ(x, 'a');
+        x = parse_getc(&buf);
+        CHECK_EQ(x, 'b');
+    }
 
-	strcpy(buf.buffer, "abcde");
-	buf.size = strlen("abcde");
-	int x = parse_getc(&buf);
-	CHECK_EQ(x, 'a');
+    TEST_SECTION("parse_read_word(buf, result)");
+    {
+        t_parse_buffer	buf;
+        init_buf_with_string(&buf, "cat ");
+        t_parse_token	tok;
+        parse_read_word(&buf, &tok);
+        CHECK_EQ(tok.length, 3);
+        CHECK(!strncmp(tok.text, "cat", 3));
+    }
 
+    TEST_SECTION("parse_get_token");
+    {
+        t_parse_buffer	buf;
+        init_buf_with_string(&buf, "cat $ABC | grep x | wc > file.txt\n");
+        t_parse_token	tok;
+        parse_get_token(&buf, &tok);
+        CHECK_EQ(tok.type, TOKTYPE_EXPANDABLE);
+        printf("tok.length = %d, tok.text = %s\n", tok.length, tok.text);
+        CHECK_EQ(tok.length, 3);
+        CHECK(!strncmp(tok.text, "cat", 3));
+    }
 
 	print_result();
 }
