501609ed455c1a09bd6a49858142c9e74e8dc1b3
Junichi Tanaka <junichi200123@gmail.com>
2021-04-04 05:30:06 +0900

    コマンドを引数付きで実行できるようにした

--- command_execution.c
+++ command_execution.c
@@ -13,6 +13,7 @@ static void sig_quit(int signo); /* Ctrl + \ */
 int	main(int argc, char **argv)
 {
 	char	*line;
+	char	**line_args;
 	pid_t	pid;
 	int		status;
 	int		gnl_status;
@@ -27,7 +28,17 @@ int	main(int argc, char **argv)
 		if ((pid = fork()) < 0){
 			printf("fork() error\n");
 		}else if (pid == 0){  /* 子プロセスの処理 */
-			execlp(line, "", (char *)0);
+			line_args = ft_split(line, ' ');
+			if (!line_args)
+			{
+				printf("ft_split() error. input: |%s|\n", line);
+				exit(127);
+			}
+			/*
+			for (int i = 0; line_args[i]; i++)
+				printf("line_args[%d]: |%s|\n", i, line_args[i]);
+				*/
+			execvp(line_args[0], line_args);
 			printf("couldn't execute %s\n", line);
 			exit(127);
 		}
