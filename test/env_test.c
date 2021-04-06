70c73eee737f955d8d1f54028132d505262d3b35
Junichi Tanaka <junichi200123@gmail.com>
2021-04-07 07:23:21 +0900

    get_env()のテストを追加

--- test/env_test.c
+++ test/env_test.c
@@ -1,7 +1,10 @@
 #include "test.h"
 #include "../env.h"
+
 int main(){
-    TEST_SECTION("split_first_c()");
+	extern char **environ;
+
+	TEST_SECTION("split_first_c()");
 	{
 		char *input = "PATH=/bin/:/usr/bin/:/home/jun/bin";
 		char **str_arr = split_first_c(input, '=');
@@ -9,10 +12,14 @@ int main(){
 		CHECK_EQ_STR(str_arr[1], "/bin/:/usr/bin/:/home/jun/bin");
 	}
 
-    TEST_SECTION("get_env()");
-    {
-		
-    }
+	TEST_SECTION("get_env()");
+	{
+		char *original0 = environ[0];
+		environ[0] = "GET_ENV_TEST=/bin/:/usr/bin/:/home/jun/bin";
+		char *kvstr = get_env("GET_ENV_TEST");
+		CHECK_EQ_STR(kvstr, environ[0]);
+		environ[0] = original0;
+	}
 
 	int fail_count = print_result();
 	return (fail_count);
