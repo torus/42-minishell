f1e029f90893b28e46dd46e65d3cbe04f108b899
Junichi Tanaka <junichi200123@gmail.com>
2021-04-07 06:01:21 +0900

    test.h に CHECK_EQ_STR() を追加

--- test/test.h
+++ test/test.h
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 #define CHECK(val) test_check((int64_t)val, #val)
 #define CHECK_EQ(actual, expected) test_check(actual == expected, #actual " == " #expected)
+#define CHECK_EQ_STR(actual, expected) test_check(strcmp(actual, expected) == 0, #actual " == " #expected)
 #define TEST_SECTION(message) printf("- " message "\n")
 
 void	test_check(int64_t val, const char *msg);
