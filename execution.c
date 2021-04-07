c2874463ae9cfe046b9cd8dbf8c92a7a2d596b19
Junichi Tanaka <junichi200123@gmail.com>
2021-04-07 10:11:21 +0900

    find_executable_file_from_path_env() を実装

--- execution.c
+++ execution.c
@@ -1,8 +1,33 @@
+#include "env.h"
 #include "execution.h"
 
 char	*find_executable_file(char *filename)
 {
-	return (NULL);
+
+}
+
+char	*find_executable_file_from_path_env(char *filename)
+{
+	char *path_env_val;
+	char **paths;
+	size_t idx;
+
+	// find "hoge:hoge" from "PATH=hoge:hoge"
+	path_env_val = get_env_val("PATH");
+	if (!path_env_val)
+		return (NULL);
+	// split path_env_val with ':'
+	paths = ft_split(path_env_val, ':');
+	if (!paths)
+		return (NULL);
+	idx = 0;
+	while (paths[idx])
+	{
+		if (find_executable_file_in_dir(filename, paths[idx]))
+			break;
+		idx++;
+	}
+	return (find_executable_file_in_dir(filename, paths[idx]));
 }
 
 int	ft_execvp(char *filename, char **argv)
--- execution.h
+++ execution.h
@@ -10,13 +10,24 @@
 
 # define MAXLINE 1000
 
+typedef struct s_command_invocation
+{
+    const char              *output_file_path; /* > file の場合 */
+    struct s_command_invocation    *piped_command; /* | command の場合（> と | は同時には指定できない） */
+    const char              *input_file_path; /* < file */
+    const char              **exec_and_args; /* ft_split で区切ったコマンドのパスと引数のリスト */
+}   t_command_invocation;
+
+
 /* Signal handlers */
 static void	sig_int(int signo);  /* Ctrl + C */
 static void	sig_quit(int signo); /* Ctrl + \ */
 
 /* Execution */
+char	*find_executable_file_in_dir(char *filename, char *dirpath);
+char	*find_executable_file_from_path_env(char *filename);
 char	*find_executable_file(char *filename);
-int	ft_execvp(char *filename, char **argv);
+int		ft_execvp(char *filename, char **argv);
 
 #endif
 
