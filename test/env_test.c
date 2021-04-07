d49dbb31f7809ecadf89078cb7d9c9ff106a37ec
Junichi Tanaka <junichi200123@gmail.com>
2021-04-07 09:15:25 +0900

    split_first_c()のテストケースを追加

--- test/env_test.c
+++ test/env_test.c
@@ -4,12 +4,22 @@
 int main(){
 	extern char **environ;
 
-	TEST_SECTION("split_first_c()");
+	TEST_SECTION("split_first_c() 普通に\"Key=Value\"");
 	{
 		char *input = "PATH=/bin/:/usr/bin/:/home/jun/bin";
 		char **str_arr = split_first_c(input, '=');
 		CHECK_EQ_STR(str_arr[0], "PATH");
 		CHECK_EQ_STR(str_arr[1], "/bin/:/usr/bin/:/home/jun/bin");
+		CHECK_EQ(str_arr[2], NULL);
+	}
+
+	TEST_SECTION("split_first_c() Valueの中に=が入っていない場合");
+	{
+		char *input = "PATH/bin/:/usr/bin/:/home/jun/bin";
+		char **str_arr = split_first_c(input, '=');
+		CHECK_EQ_STR(str_arr[0], "PATH/bin/:/usr/bin/:/home/jun/bin");
+		CHECK_EQ(str_arr[1], NULL);
+		CHECK_EQ(str_arr[2], NULL);
 	}
 
 	TEST_SECTION("get_env()");
