e04e942b77732299119f4c775646075d766c4ffb
Junichi Tanaka <junichi200123@gmail.com>
2021-04-09 07:00:32 +0900

    CHECK_FALSE() を削除

--- test/test.h
+++ test/test.h
@@ -7,8 +7,7 @@
 #define CHECK(val) test_check((int64_t)val, #val)
 #define CHECK_EQ(actual, expected) test_check(actual == expected, #actual " == " #expected)
 #define CHECK_EQ_STR(actual, expected) test_check(strcmp(actual, expected) == 0, #actual " == " #expected)
-#define CHECK_TRUE(actual) test_check(actual, #actual " == true")
-#define CHECK_FALSE(actual) test_check(actual, #actual " == false")
+#define CHECK_TRUE(actual) test_check(actual, #actual)
 #define TEST_SECTION(message) printf("- " message "\n")
 
 void	test_check(int64_t val, const char *msg);
