6a2f8325fe513d1e7c95ed88a2cc5ce96c8ee975
Junichi Tanaka <junichi200123@gmail.com>
2021-04-09 05:36:08 +0900

    path_test を追加

---
+++ test/path_test.c
@@ -0,0 +1,18 @@
+#include "test.h"
+#include "../execution.h"
+
+int main(){
+	TEST_SECTION("path_join() 最後スラッシュ無し");
+	{
+		char *fullpath = path_join("/usr/bin", "cat");
+		CHECK_EQ_STR(fullpath, "/usr/bin/cat");
+		free(fullpath);
+	}
+
+	TEST_SECTION("path_join() 最後スラッシュあり");
+	{
+		char *fullpath = path_join("/usr/bin/", "cat");
+		CHECK_EQ_STR(fullpath, "/usr/bin//cat");
+		free(fullpath);
+	}
+}
