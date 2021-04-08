d1252f603131f3f4d64e57e766be949a1c0dca00
Junichi Tanaka <junichi200123@gmail.com>
2021-04-09 06:15:55 +0900

    CHECK_TRUE() と CHECK_FALSE() を追加

--- test/test.h
+++ test/test.h
@@ -7,6 +7,8 @@
 #define CHECK(val) test_check((int64_t)val, #val)
 #define CHECK_EQ(actual, expected) test_check(actual == expected, #actual " == " #expected)
 #define CHECK_EQ_STR(actual, expected) test_check(strcmp(actual, expected) == 0, #actual " == " #expected)
+#define CHECK_TRUE(actual) test_check(actual, #actual " == true")
+#define CHECK_FALSE(actual) test_check(actual, #actual " == false")
 #define TEST_SECTION(message) printf("- " message "\n")
 
 void	test_check(int64_t val, const char *msg);
