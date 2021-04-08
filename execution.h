d936c288bd90df8f7e7047c7141834cdfa81b713
Junichi Tanaka <junichi200123@gmail.com>
2021-04-09 05:15:57 +0900

    find_executable_file() を execution.h から削除

--- execution.h
+++ execution.h
@@ -28,7 +28,6 @@ static void	sig_quit(int signo); /* Ctrl + \ */
 /* Execution */
 char	*find_executable_file_in_dir(char *filename, char *dirpath);
 char	*find_executable_file_from_path_env(char *filename);
-char	*find_executable_file(char *filename);
 int		ft_execvp(char *filename, char **argv);
 
 /* path */
