2e8555d71504515da2a79e43209b344f99c6013d
Junichi Tanaka <junichi200123@gmail.com>
2021-04-09 05:36:59 +0900

    exec_testを追加

---
+++ test/exec_test.c
@@ -0,0 +1,25 @@
+#include "test.h"
+#include "../execution.h"
+
+int main(){
+	TEST_SECTION("find_executable_file_in_dir(\"cat\", \"/usr/bin\"");
+	{
+		char *fullpath = find_executable_file_in_dir("cat", "/usr/bin");
+		CHECK_EQ_STR(fullpath, "/usr/bin/cat");
+		free(fullpath);
+	}
+
+	TEST_SECTION("find_executable_file_in_dir(\"cat\", \"/usr/bin/\"");
+	{
+		char *fullpath = find_executable_file_in_dir("cat", "/usr/bin/");
+		CHECK_EQ_STR(fullpath, "/usr/bin/cat");
+		free(fullpath);
+	}
+
+	TEST_SECTION("find_executable_file_in_dir(\"cat\", \"/\"");
+	{
+		char *fullpath = find_executable_file_in_dir("cat", "/usr/bin/");
+		CHECK_EQ(fullpath, NULL);
+		free(fullpath);
+	}
+}
