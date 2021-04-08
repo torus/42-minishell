1f414f09bb2bf12a3c2e8ea6c71dabb25d1339d3
Junichi Tanaka <junichi200123@gmail.com>
2021-04-09 03:51:32 +0900

    ls.c内でstat()の使用例を追加

--- ls.c
+++ ls.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <stdio.h>
 
@@ -7,6 +8,7 @@ int main(int argc, char **argv)
 {
 	DIR *dp;
 	struct dirent *dirp;
+	struct stat buf;
 
 	if (argc != 2)
 		return (1);
@@ -14,10 +16,22 @@ int main(int argc, char **argv)
 		return (1);
 	while ((dirp = readdir(dp)) != NULL)
 	{
-		// ファイル名
-		printf("filename: %s\n", dirp->d_name);
-		// i_node番号
-		printf("\ti_node: %lu\n", dirp->d_ino);
+		// display filename
+		printf("%s\n", dirp->d_name);
+		// get file status
+		if (stat(dirp->d_name, &buf) == -1)
+		{
+			printf("\terror: stat()\n");
+			continue;
+		}
+		if (S_ISREG(buf.st_mode))
+			printf("\ttype: Regular\n");
+		else if (S_ISDIR(buf.st_mode))
+			printf("\ttype: Directory\n");
+		else if (S_ISLNK(buf.st_mode))
+			printf("\ttype: Link\n");
+		else
+			printf("\ttype: Other\n");
 	}
 	closedir(dp);
 	return (0);
