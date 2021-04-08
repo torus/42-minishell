91e1cd59563407921c0fe1f14d14a201e1049a95
Junichi Tanaka <junichi200123@gmail.com>
2021-04-09 05:47:13 +0900

    exec_test を修正

--- test/exec_test.c
+++ test/exec_test.c
@@ -12,13 +12,13 @@ int main(){
 	TEST_SECTION("find_executable_file_in_dir(\"cat\", \"/usr/bin/\")");
 	{
 		char *fullpath = find_executable_file_in_dir("cat", "/usr/bin/");
-		CHECK_EQ_STR(fullpath, "/usr/bin/cat");
+		CHECK_EQ_STR(fullpath, "/usr/bin//cat");
 		free(fullpath);
 	}
 
 	TEST_SECTION("find_executable_file_in_dir(\"cat\", \"/\")");
 	{
-		char *fullpath = find_executable_file_in_dir("cat", "/usr/bin/");
+		char *fullpath = find_executable_file_in_dir("cat", "/");
 		CHECK_EQ(fullpath, NULL);
 		free(fullpath);
 	}
