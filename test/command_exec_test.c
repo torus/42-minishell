b483524f77fb6cb55933e7e9675085c25b47385b
Junichi Tanaka <junichi200123@gmail.com>
2021-04-09 15:16:47 +0900

    区別が付くようにテストケースごとに違うファイルを渡すようにした

--- test/command_exec_test.c
+++ test/command_exec_test.c
@@ -29,23 +29,36 @@ int main(){
 		remove("output.txt");
     }
 
-	/*
-    TEST_SECTION("cat < /etc/passwd");
+    TEST_SECTION("cat < Makefile");
     {
 		t_command_invocation command;
 		command.output_file_path = NULL;
 		command.piped_command = NULL;
-		command.input_file_path = "/etc/passwd";
+		command.input_file_path = "Makefile";
 		command.exec_and_args = (const char**)ft_split("cat", ' ');
 
 		command_execution(&command);
 		free_ptrarr((void**)command.exec_and_args);
     }
 
-    TEST_SECTION("cat /etc/passwd | wc > wc_tmp.txt");
+    TEST_SECTION("cat < test.h > output.txt");
+    {
+		t_command_invocation command;
+		command.output_file_path = "output.txt";
+		command.piped_command = NULL;
+		command.input_file_path = "test.h";
+		command.exec_and_args = (const char**)ft_split("cat", ' ');
+
+		command_execution(&command);
+		free_ptrarr((void**)command.exec_and_args);
+		CHECK_TRUE(system("diff test.h output.txt") == 0);
+		remove("output.txt");
+    }
+
+    TEST_SECTION("cat /etc/passwd | wc > output.txt");
     {
 		t_command_invocation wc_command;
-		wc_command.output_file_path = "wc_tmp.txt";
+		wc_command.output_file_path = "output.txt";
 		wc_command.piped_command = NULL;
 		wc_command.input_file_path = NULL;
 		wc_command.exec_and_args = (const char**)ft_split("wc", ' ');
@@ -59,8 +72,9 @@ int main(){
 		command_execution(&cat_command);
 		free_ptrarr((void**)cat_command.exec_and_args);
 		free_ptrarr((void**)wc_command.exec_and_args);
+		CHECK_TRUE(system("diff <(cat /etc/passwd | wc) output.txt") == 0);
+		remove("output.txt");
     }
-	*/
 
 	int fail_count = print_result();
 	return (fail_count);
