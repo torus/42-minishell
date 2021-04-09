a5c1b9caa505832ffa6e1dfda9f36f0276af6b79
Junichi Tanaka <junichi200123@gmail.com>
2021-04-09 15:06:35 +0900

    exec.c のデバッグ用コメントを削除

--- exec.c
+++ exec.c
@@ -80,7 +80,6 @@ int	ft_execvp(char *filename, char **argv)
 		executable_path = filename;
 	else
 		executable_path = find_executable_file_from_path_env(filename);
-	printf("executable_path: %s\n", executable_path);  // debug
 	execve(executable_path, argv, environ);
 	return (ERROR);
 }
