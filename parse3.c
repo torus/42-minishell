e30c7b48ebef14d6b25b9be3606f300745e4719b
Toru Hisai <toru@torus.jp>
2021-04-09 18:52:23 +0900

    クォートなしの文字列のパーサが一応動く。

--- parse3.c
+++ parse3.c
@@ -51,26 +51,42 @@ int	parse_string(
 	t_parse_buffer *buf, t_parse_ast_node **node, t_parse_token *tok)
 {
 	t_parse_ast_node	*new_node;
-	t_parse_ast_node	*next_node;
 	t_parse_node_string	*string;
+	t_parse_node_string	*next_string;
 	char				*text;
 
 	if (tok->type != TOKTYPE_EXPANDABLE)
 		return (PARSE_KO);
 
+
+	new_node = malloc(sizeof(t_parse_ast_node));
+	if (!new_node)
+		parse_fatal_error();
+	new_node->type = ASTNODE_STRING;
+
+	string = NULL;
+	next_string = NULL;
 	while (tok->type == TOKTYPE_EXPANDABLE)
 	{
-		new_node = malloc(sizeof(t_parse_ast_node));
-		string = malloc(sizeof(t_parse_node_string));
+		next_string = malloc(sizeof(t_parse_node_string));
 		text = malloc(tok->length + 1);
-		if (!new_node || !string || !text)
+		if (!next_string || !text)
 			parse_fatal_error();
+
+		next_string->type = tok->type;
 
 		ft_memcpy(text, tok->text, tok->length);
 		text[tok->length] = '\0';
+		next_string->text = text;
+		if (string)
+			string->next = next_string;
+		string = next_string;
 
 		parse_get_token(buf, tok);
 	}
+	next_string->next = NULL;
+	new_node->content.string = next_string;
+	*node = new_node;
 	return (PARSE_OK);
 }
 
@@ -84,7 +100,7 @@ int	parse_redirection(
 	if (tok->type != TOKTYPE_INPUT_REDIRECTION)
 		return (PARSE_KO);
 
-	parse_get_token(&buf, tok);
+	parse_get_token(buf, tok);
 	if (parse_string(buf, &str_node, tok) == PARSE_OK)
 	{
 		new_node = malloc(sizeof(t_parse_ast_node));
