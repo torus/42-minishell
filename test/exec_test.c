093c0b60f1b4d25d15631243b2c94cfe06ee54ca
Junichi Tanaka <junichi200123@gmail.com>
2021-04-10 18:31:03 +0900

    exec_testにテストケースを追加

--- test/exec_test.c
+++ test/exec_test.c
@@ -58,6 +58,15 @@ int main(){
 		restore_env_var();
 	}
 
+	TEST_SECTION("find_executable_file_from_path_env() PATH内の最後のパスに実行可能ファイルが含まれている");
+	{
+		set_env_var("PATH=/home/jun/bin:/usr/bin");
+		char *fullpath = find_executable_file_from_path_env("cat");
+		CHECK_EQ_STR(fullpath, "/usr/bin//cat");
+		free(fullpath);
+		restore_env_var();
+	}
+
 	TEST_SECTION("find_executable_file_from_path_env() PATH内に実行可能ファイルが含まれていない");
 	{
 		set_env_var("PATH=/usr/bin/:/home/jun/bin");
