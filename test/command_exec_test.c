edf7be678a14f38efa4927a847670336c8f5adfe
Junichi Tanaka <junichi200123@gmail.com>
2021-04-12 09:35:49 +0900

    パイプのテストを修正

--- test/command_exec_test.c
+++ test/command_exec_test.c
@@ -92,7 +92,7 @@ int main(){
 		t_command_invocation cat_command;
 		cat_command.output_file_path = NULL;
 		cat_command.piped_command = &wc_command;
-		cat_command.input_file_path = "/etc/passwd";
+		cat_command.input_file_path = NULL;
 		cat_command.exec_and_args = (const char**)ft_split("cat /etc/passwd", ' ');
 
 		command_execution(&cat_command);
@@ -102,34 +102,59 @@ int main(){
 		remove("output.txt");
     }
 
-	/*
-    TEST_SECTION("cat /etc/passwd | wc | cat > output.txt  パイプが2つの場合");
+    TEST_SECTION("cat /etc/passwd | wc | md5sum > output.txt  パイプが2つの場合");
     {
-		t_command_invocation cat_last_command;
-		cat_last_command.output_file_path = "output.txt";
-		cat_last_command.piped_command = NULL;
-		cat_last_command.input_file_path = NULL;
-		cat_last_command.exec_and_args = (const char**)ft_split("cat", ' ');
+		t_command_invocation md5sum_command;
+		md5sum_command.output_file_path = "output.txt";
+		md5sum_command.piped_command = NULL;
+		md5sum_command.input_file_path = NULL;
+		md5sum_command.exec_and_args = (const char**)ft_split("md5sum", ' ');
 
 		t_command_invocation wc_command;
 		wc_command.output_file_path = NULL;
-		wc_command.piped_command = &cat_last_command;
+		wc_command.piped_command = &md5sum_command;
 		wc_command.input_file_path = NULL;
 		wc_command.exec_and_args = (const char**)ft_split("wc", ' ');
 
 		t_command_invocation cat_command;
 		cat_command.output_file_path = NULL;
 		cat_command.piped_command = &wc_command;
-		cat_command.input_file_path = "/etc/passwd";
+		cat_command.input_file_path = NULL;
 		cat_command.exec_and_args = (const char**)ft_split("cat /etc/passwd", ' ');
 
 		command_execution(&cat_command);
 		free_ptrarr((void**)cat_command.exec_and_args);
 		free_ptrarr((void**)wc_command.exec_and_args);
-		CHECK(system("diff <($(cat /etc/passwd | wc | cat)) output.txt") == 0);
+		CHECK(system("cat /etc/passwd | wc | md5sum > expected.txt ; diff expected.txt output.txt") == 0);
 		remove("output.txt");
     }
-	*/
+
+    TEST_SECTION("cat /etc/passwd > /dev/null | wc | md5sum > output.txt  パイプの途中でリダイレクトがある");
+    {
+		t_command_invocation md5sum_command;
+		md5sum_command.output_file_path = "output.txt";
+		md5sum_command.piped_command = NULL;
+		md5sum_command.input_file_path = NULL;
+		md5sum_command.exec_and_args = (const char**)ft_split("md5sum", ' ');
+
+		t_command_invocation wc_command;
+		wc_command.output_file_path = NULL;
+		wc_command.piped_command = &md5sum_command;
+		wc_command.input_file_path = NULL;
+		wc_command.exec_and_args = (const char**)ft_split("wc", ' ');
+
+		t_command_invocation cat_command;
+		cat_command.output_file_path = "/dev/null";
+		cat_command.piped_command = &wc_command;
+		cat_command.input_file_path = NULL;
+		cat_command.exec_and_args = (const char**)ft_split("cat /etc/passwd", ' ');
+
+		command_execution(&cat_command);
+		free_ptrarr((void**)cat_command.exec_and_args);
+		free_ptrarr((void**)wc_command.exec_and_args);
+		CHECK(system("cat /etc/passwd > /dev/null | wc | md5sum > expected.txt ; diff expected.txt output.txt") == 0);
+		remove("output.txt");
+    }
 
 	int fail_count = print_result();
 	return (fail_count);
