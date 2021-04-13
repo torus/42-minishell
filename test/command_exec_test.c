f9d9020888d591a3744ff6bb42f6612d16f15701
Junichi Tanaka <junichi200123@gmail.com>
2021-04-13 12:03:39 +0900

    テストケースを追加

--- test/command_exec_test.c
+++ test/command_exec_test.c
@@ -156,6 +156,33 @@ int main(){
 		remove("output.txt");
     }
 
+    TEST_SECTION("cat test.h | wc < test.c | md5sum > output.txt  パイプの真ん中のコマンドでリダイレクトがある");
+    {
+		t_command_invocation md5sum_command;
+		md5sum_command.output_file_path = "output.txt";
+		md5sum_command.piped_command = NULL;
+		md5sum_command.input_file_path = NULL;
+		md5sum_command.exec_and_args = (const char**)ft_split("md5sum", ' ');
+
+		t_command_invocation wc_command;
+		wc_command.output_file_path = NULL;
+		wc_command.piped_command = &md5sum_command;
+		wc_command.input_file_path = "test.c";
+		wc_command.exec_and_args = (const char**)ft_split("wc", ' ');
+
+		t_command_invocation cat_command;
+		cat_command.output_file_path = NULL;
+		cat_command.piped_command = &wc_command;
+		cat_command.input_file_path = NULL;
+		cat_command.exec_and_args = (const char**)ft_split("cat test.h", ' ');
+
+		cmd_command_execution(&cat_command);
+		free_ptrarr((void**)cat_command.exec_and_args);
+		free_ptrarr((void**)wc_command.exec_and_args);
+		CHECK(system("cat test.h | wc < test.c | md5sum > expected.txt ; diff expected.txt output.txt") == 0);
+		remove("output.txt");
+    }
+
 	int fail_count = print_result();
 	return (fail_count);
 }
