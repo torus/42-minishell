b82d1a4061bd16a852d97f6858fc4a2a4cd16671
Junichi Tanaka <junichi200123@gmail.com>
2021-04-17 02:25:24 +0900

    cmd_print_command()を削除

--- cmd_exec_commands.c
+++ cmd_exec_commands.c
@@ -2,25 +2,6 @@
 #include "execution.h"
 
 /*
- * print t_command_invocation's attributes
- */
-static void	cmd_print_command(t_command_invocation *command)
-{
-	size_t	i;
-
-	printf("output_file_path: %s\n", command->output_file_path);
-	printf("input_file_path: %s\n", command->input_file_path);
-	printf("command: ");
-	i = 0;
-	while (command->exec_and_args[i])
-		printf("%s ", command->exec_and_args[i++]);
-	printf("\n");
-	printf("piped_command: %p\n", command->piped_command);
-	if (command->piped_command)
-		cmd_print_command(command->piped_command);
-}
-
-/*
  * fork and execute commands.
  *
  * return: status of last command
@@ -33,7 +14,6 @@ int	cmd_exec_commands(t_command_invocation *command)
 	int		pipe_prev_fd[2];
 	t_list	*pid_lst;
 
-	cmd_print_command(command);
 	cmd_init_pipe_fd(pipe_prev_fd, STDIN_FILENO, -1);
 	pid_lst = NULL;
 	while (command)
