61f2514c3b8215eeb550e081531f1cd0be46fe7d
Junichi Tanaka <junichi200123@gmail.com>
2021-04-17 02:08:25 +0900

    プロセスが最初のプロセスかどうか判定する条件式を変更

--- cmd_exec_commands.c
+++ cmd_exec_commands.c
@@ -47,7 +47,7 @@ int	cmd_command_execution(t_command_invocation *command)
 			return (put_err_msg_and_ret("error fork()"));
 		else if (pid > 0)
 		{
-			if (pipe_prev_fd[0] != STDIN_FILENO)
+			if (pipe_prev_fd[1] != -1)
 				cmd_close_pipe(pipe_prev_fd);
 			cmd_copy_pipe(pipe_prev_fd, pipe_fd);
 			if (!cmd_lstadd_back_pid(&pid_lst, pid))
