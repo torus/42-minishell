2b4123766281f66e81a879e8e435222c4445f5a5
Junichi Tanaka <junichi200123@gmail.com>
2021-04-20 01:24:51 +0900

    add_cmdinvo() を作成

--- cmd_cmd_invocation.c
+++ cmd_cmd_invocation.c
@@ -7,6 +7,7 @@ t_command_invocation *init_cmdinvo(const char *output_file_path,
 	const char *input_file_path, const char **exec_and_args )
 {
 	t_command_invocation *cmdinvo;
+
 	cmdinvo = malloc(sizeof(t_command_invocation));
 	if (!cmdinvo)
 		return (NULL);
@@ -14,4 +15,15 @@ t_command_invocation *init_cmdinvo(const char *output_file_path,
 	cmdinvo->output_file_path = output_file_path;
 	cmdinvo->exec_and_args = exec_and_args;
 	return (cmdinvo);
+}
+
+/*
+** cmd->pipe_command にコマンドを追加
+*/
+t_command_invocation *add_cmdinvo(t_command_invocation *cmds, t_command_invocation *newcmd)
+{
+	while (cmds->piped_command)
+		cmds = cmds->piped_command;
+	cmds->piped_command = newcmd;
+	return (newcmd);
 }
