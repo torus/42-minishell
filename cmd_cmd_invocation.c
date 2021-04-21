63ca9318dbfb39b58e0f55a25fc42261fec9ef6e
Junichi Tanaka <junichi200123@gmail.com>
2021-04-21 23:43:15 +0900

    cmd_cmd_invocation.c Normエラー修正

--- cmd_cmd_invocation.c
+++ cmd_cmd_invocation.c
@@ -6,7 +6,7 @@
 t_command_invocation	*cmd_init_cmdinvo(const char *output_file_path,
 	const char *input_file_path, const char **exec_and_args, unsigned int flags)
 {
-	t_command_invocation *cmdinvo;
+	t_command_invocation	*cmdinvo;
 
 	cmdinvo = malloc(sizeof(t_command_invocation));
 	if (!cmdinvo)
@@ -22,7 +22,8 @@ t_command_invocation	*cmd_init_cmdinvo(const char *output_file_path,
 /*
 ** cmd->pipe_command にコマンドを追加
 */
-t_command_invocation *cmd_add_cmdinvo(t_command_invocation **cmds, t_command_invocation *newcmd)
+t_command_invocation	*cmd_add_cmdinvo(t_command_invocation **cmds,
+	t_command_invocation *newcmd)
 {
 	t_command_invocation	*current_cmd;
 
