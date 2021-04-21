3cc7466fbfe5cf802a773c2af830440c76381202
Junichi Tanaka <junichi200123@gmail.com>
2021-04-22 00:34:41 +0900

    convert_ast2cmdinvo.c Normエラー修正

--- convert_ast2cmdinvo.c
+++ convert_ast2cmdinvo.c
@@ -1,7 +1,7 @@
 #include "minishell.h"
 
-/* string node を処理する
-** 
+/* set values of command->exec_and_args based on string_node
+**
 ** string ::=
 **       expandable <no_space> string
 **     | expandable
@@ -10,27 +10,32 @@
 **     | expandable_quoted <no_space> string
 **     | expandable_quoted
 */
-int cmd_process_string_node(t_parse_node_string *string_node, t_command_invocation *command)
+int	cmd_process_string_node(t_parse_node_string *string_node,
+	t_command_invocation *command)
 {
-	const char **strarr = (const char **)ptrarr_add_ptr(
-		(void **)command->exec_and_args, (void *)string_node->text);
+	const char	**strarr;
+
+	strarr = (const char **)ptrarr_add_ptr((void **)command->exec_and_args,
+			(void *)string_node->text);
 	if (!strarr)
-		return	(ERROR);
+		return (ERROR);
 	free((void **)command->exec_and_args);
 	command->exec_and_args = strarr;
 	return (0);
 }
 
-/* redirection node を処理する
+/* set values of command->input_file_path or output_file_path
+** based on redirection_node
 **
 ** redirection ::=
 **       "<" string
 **     | ">" string
 **     | ">>" string
 */
-int cmd_process_redirection_node(t_parse_node_redirection *redirection_node, t_command_invocation *command)
+int	cmd_process_redirection_node(t_parse_node_redirection *redirection_node,
+	t_command_invocation *command)
 {
-	int	redirection_type;
+	int			redirection_type;
 	const char	*text;
 
 	redirection_type = redirection_node->type;
@@ -50,7 +55,7 @@ int cmd_process_redirection_node(t_parse_node_redirection *redirection_node, t_c
 	return (0);
 }
 
-/* arguments node の string と redirection を処理する
+/* parse argument_node and set values of command
 **
 ** arguments ::=
 **       redirection
@@ -58,28 +63,28 @@ int cmd_process_redirection_node(t_parse_node_redirection *redirection_node, t_c
 **     | string
 **     | string arguments
 */
-int cmd_process_arguments_node(t_parse_node_arguments *args_node, t_command_invocation *command)
+int	cmd_process_arguments_node(t_parse_node_arguments *args_node,
+	t_command_invocation *command)
 {
-	// string
 	if (args_node->string_node)
-		cmd_process_string_node(args_node->string_node->content.string, command);
-	// redirection
+		cmd_process_string_node(
+			args_node->string_node->content.string, command);
 	else if (args_node->redirection_node)
-		cmd_process_redirection_node(args_node->redirection_node->content.redirection, command);
+		cmd_process_redirection_node(
+			args_node->redirection_node->content.redirection, command);
 	else
 		return (1);
 	return (0);
 }
 
-/*
-** command を受け取って t_command_invocation を返す
+/* convert ast command_node to command_invocation object
 **
 ** command ::=
 **     arguments
 **   | (bonus) "(" sequencial_commands ")"
 **   | (bonus) "(" sequencial_commands delimiter ")"
 */
-t_command_invocation *cmd_ast_cmd2cmdinvo(t_parse_node_command *cmd_node)
+t_command_invocation	*cmd_ast_cmd2cmdinvo(t_parse_node_command *cmd_node)
 {
 	t_command_invocation	*command;
 	t_parse_node_arguments	*args_node;
@@ -87,8 +92,6 @@ t_command_invocation *cmd_ast_cmd2cmdinvo(t_parse_node_command *cmd_node)
 	command = cmd_init_cmdinvo(NULL, NULL, NULL, 0);
 	if (!command)
 		return (NULL);
-
-	// argumentsの処理
 	args_node = cmd_node->arguments_node->content.arguments;
 	while (args_node)
 	{
@@ -103,8 +106,8 @@ t_command_invocation *cmd_ast_cmd2cmdinvo(t_parse_node_command *cmd_node)
 	return (command);
 }
 
-// pipcmds を受け取って t_command_invocation を返す
-/*
+/* convert ast command_node to command_invocation object
+**
 ** pipcmds: piped_commands
 ** piped_commands ::=
 **       command "|" piped_commands
