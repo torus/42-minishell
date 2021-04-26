0f9e40ec14eef87ca71cd5aabb0eca28a17eecab
Junichi Tanaka <junichi200123@gmail.com>
2021-04-27 03:12:15 +0900

    split_expanded_str() ができた

--- convert_ast2cmdinvo.c
+++ convert_ast2cmdinvo.c
@@ -46,6 +46,84 @@ char	*string_node2string(t_parse_node_string *string_node)
 }
 
 /*
+ * 環境変数展開した文字列を分解して返す
+ */
+char	**split_expanded_str(char *str)
+{
+	char	**result;
+	char	quote_char;  // ' or " or \0
+
+	quote_char = '\0';
+	result = NULL;
+	// |     hogeabc def"hoge hoge"'$ABC'def abc |
+	while (*str)
+	{
+		// 空白飛ばし
+		while (*str && *str == ' ')
+			str++;
+		if (!*str)
+			break ;
+		int		len = 0;
+		char	*text = NULL;
+		while (str[len])
+		{
+			// クオートの中じゃない時の空白は区切り
+			if (!quote_char && str[len] == ' ')
+			{
+				char *tmp = text;
+				char *tmp2 = ft_substr(str, 0, len);
+				str += len;
+				len = 0;
+				if (tmp)
+					text = ft_strjoin(tmp, tmp2);
+				else
+					text = ft_strdup(tmp2);
+				free(tmp);
+				free(tmp2);
+				break;
+			}
+			if ((str[len] == '\'' || str[len] == '\"') && !(len > 0 && str[len - 1] == '\\'))
+			{
+				if (quote_char)
+				{
+					quote_char = '\0';
+					char *tmp = text;
+					char *tmp2 = ft_substr(str, 0, len);
+					str += len + 1;  // クオートの次の文字
+					len = 0;
+					if (tmp)
+						text = ft_strjoin(tmp, tmp2);
+					else
+						text = ft_strdup(tmp2);
+					free(tmp);
+					free(tmp2);
+				}
+				else
+				{
+					quote_char = str[len];
+					char *tmp = text;
+					char *tmp2 = ft_substr(str, 0, len);
+					str += len + 1;  // クオートの次の文字
+					len = 0;
+					if (tmp)
+						text = ft_strjoin(tmp, tmp2);
+					else
+						text = ft_strdup(tmp2);
+					free(tmp);
+					free(tmp2);
+				}
+			}
+			else
+				len++;
+		}
+		char	**tmp = result;
+		result = (char **)ptrarr_add_ptr((void **)tmp, text);
+		free(tmp);
+	}
+	return (result);
+}
+
+/*
  * in: abc"x y""abc def"  // abc: expandable, x y: expandable_quoted, abc def: expandable_quoted  // abc"x y""abc def"
  * out: "abcx yabc def"
  *
@@ -64,72 +142,8 @@ char **expand_string_node(t_parse_node_string *string_node)
 	// 元に戻した文字列内の環境変数を展開する
 	str = expand_env_var(tmp);  // out: |" abc def "\\'\'$ABC'|
 	free(tmp);
-	printf("expanded str: %s\n", str);
-
-	// 展開された文字列を元にsplitする
-	int		i;
-	char	**result;
-	char	quote_char;  // ' or " or \0
-	int		start_idx;
-	char	*text;
-
-	i = 0;
-	quote_char = '\0';
-	result = NULL;
-	start_idx = 0;
-	text = NULL;
-	// " abc def"\\'\'$ABC'
-	// |     hogeabc def"hoge hoge"'$ABC'|
-	while (str[i])
-	{
-		// 空白飛ばし
-		while (!quote_char && str[i] && str[i] <= ' ' && str[i] > '~')
-			i++;
-		if (!str[i])
-			break;
-		start_idx = i;
-		if (str[i] == '\'' || str[i] == '\"')
-		{
-			i++;  // クオートの次の文字から
-			start_idx = i;
-			quote_char = str[i];
-		}
-		// 空白 か クオート にぶつかるまで  (クオートの中では空白飛ばさない)
-		while (!quote_char && str[i] && str[i] > ' ' && str[i] <= '~' && !(str[i] == '\'' || str[i] == '\"'))
-			i++;
-		if (str[i] == '\'' || str[i] == '\"')
-		{
-			i++;  // クオートの次の文字から
-			start_idx = i;
-			quote_char = str[i];
-		}
-		// クオートの中で次のクオートにぶつかるまで
-		while (str[i] && quote_char && (str[i] != quote_char || (str[i] == quote_char && str[i-1] != '\\')))
-			i++;
-		quote_char = '\0';
-		// 最後終わった時に空白じゃなかったら次の文字列と繋げる
-		if (str[i] > ' ' || str[i] <= '~')
-		{
-			char *tmp1 = text;
-			char *tmp2 = ft_substr(str, start_idx, i - start_idx);
-			if (text)
-				text = ft_strjoin(tmp1, tmp2);
-			else
-				text = ft_strdup(tmp2);
-			free(tmp1);
-			free(tmp2);
-		}
-		else
-		{
-			char **tmp1 = result;
-			char *tmp2 = ft_substr(str, start_idx, i - start_idx);
-			printf("add: %s\n", tmp2);
-			result = (char **)ptrarr_add_ptr((void**)tmp1, tmp2);
-			free(tmp1);
-		}
-		i++;
-	}
-	return (result);
+	printf("expanded str: |%s|\n", str);
+	return (split_expanded_str(str));
 }
 
 /* set values of command->exec_and_args based on string_node
