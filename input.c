66da2595209278c7dc90680de3546de2a77d7a94
Toru Hisai <toru@torus.jp>
2021-04-29 19:46:57 +0900

    端末入力のテスト。

--- input.c
+++ input.c
@@ -68,85 +68,6 @@ err_sys(const char *fmt, ...)
 }
 
 int
-tty_raw(int fd)		/* put terminal into a raw mode */
-{
-	int				err;
-	struct termios	buf;
-
-	if (ttystate != RESET) {
-		errno = EINVAL;
-		return(-1);
-	}
-	if (tcgetattr(fd, &buf) < 0)
-		return(-1);
-	save_termios = buf;	/* structure copy */
-
-	/*
-	 * Echo off, canonical mode off, extended input
-	 * processing off, signal chars off.
-	 */
-	buf.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-
-	/*
-	 * No SIGINT on BREAK, CR-to-NL off, input parity
-	 * check off, don't strip 8th bit on input, output
-	 * flow control off.
-	 */
-	buf.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-
-	/*
-	 * Clear size bits, parity checking off.
-	 */
-	buf.c_cflag &= ~(CSIZE | PARENB);
-
-	/*
-	 * Set 8 bits/char.
-	 */
-	buf.c_cflag |= CS8;
-
-	/*
-	 * Output processing off.
-	 */
-	buf.c_oflag &= ~(OPOST);
-
-	/*
-	 * Case B: 1 byte at a time, no timer.
-	 */
-	buf.c_cc[VMIN] = 1;
-	buf.c_cc[VTIME] = 0;
-	if (tcsetattr(fd, TCSAFLUSH, &buf) < 0)
-		return(-1);
-
-	/*
-	 * Verify that the changes stuck.  tcsetattr can return 0 on
-	 * partial success.
-	 */
-	if (tcgetattr(fd, &buf) < 0) {
-		err = errno;
-		tcsetattr(fd, TCSAFLUSH, &save_termios);
-		errno = err;
-		return(-1);
-	}
-	if ((buf.c_lflag & (ECHO | ICANON | IEXTEN | ISIG)) ||
-	  (buf.c_iflag & (BRKINT | ICRNL | INPCK | ISTRIP | IXON)) ||
-	  (buf.c_cflag & (CSIZE | PARENB | CS8)) != CS8 ||
-	  (buf.c_oflag & OPOST) || buf.c_cc[VMIN] != 1 ||
-	  buf.c_cc[VTIME] != 0) {
-		/*
-		 * Only some of the changes were made.  Restore the
-		 * original settings.
-		 */
-		tcsetattr(fd, TCSAFLUSH, &save_termios);
-		errno = EINVAL;
-		return(-1);
-	}
-
-	ttystate = RAW;
-	ttysavefd = fd;
-	return(0);
-}
-
-int
 tty_cbreak(int fd)	/* put terminal into a cbreak mode */
 {
 	int				err;
@@ -212,18 +133,6 @@ main(void)
 	if (signal(SIGTERM, sig_catch) == SIG_ERR)
 		err_sys("signal(SIGTERM) error");
 
-	if (tty_raw(STDIN_FILENO) < 0)
-		err_sys("tty_raw error");
-	printf("Enter raw mode characters, terminate with DELETE\n");
-	while ((i = read(STDIN_FILENO, &c, 1)) == 1) {
-		if ((c &= 255) == 0177)		/* 0177 = ASCII DELETE */
-			break;
-		printf("%x\n", c);
-	}
-	if (tty_reset(STDIN_FILENO) < 0)
-		err_sys("tty_reset error");
-	if (i <= 0)
-		err_sys("read error");
 	if (tty_cbreak(STDIN_FILENO) < 0)
 		err_sys("tty_cbreak error");
 	printf("\nEnter cbreak mode characters, terminate with SIGINT\n");
