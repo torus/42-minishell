15aeae18779f97bfea6eaf91eb1980aefdd98f9c
Junichi Tanaka <junichi200123@gmail.com>
2021-04-30 11:48:04 +0900

    sigint_sighandler() の雛形を作成

--- signal.c
+++ signal.c
@@ -1,8 +1,15 @@
 #include "minishell.h"
 
+static void sigint_sighandler(int sig)
+{
+	printf("receive SIGINT\n");
+}
+
 int	setup_signal_handlers(void)
 {
 	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
+		return (-1);
+	if (signal(SIGINT, sigint_sighandler) == SIG_ERR)
 		return (-1);
 	return (0);
 }
