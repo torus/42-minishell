d6376ad078ee4986204b05f1e5f6b3286a01f791
Junichi Tanaka <junichi200123@gmail.com>
2021-05-01 19:32:09 +0900

    expand_and_add_env() を実装

--- env_setter.c
+++ env_setter.c
@@ -46,12 +46,41 @@ static int	update_env(const char *name, const char *value, int rewrite)
 }
 
 /*
+ * environ より+2大きい配列を確保して環境変数を追加する.
+ */
+static int	expand_and_add_env(const char *name, const char *value)
+{
+	extern char	**environ;
+	char		**new_environ;
+	int			environ_len;
+
+	environ_len = ptrarr_len((void **)environ);
+	new_environ = ft_calloc(environ_len + 2, sizeof(char *));
+	if (!new_environ)
+		return (ERROR);
+	ft_memcpy(new_environ, environ, sizeof(char *) * environ_len);
+	new_environ[environ_len] = generate_kvstr(name, value);
+	// kvstrの生成に失敗
+	if (!new_environ[environ_len])
+	{
+		free_ptrarr((void **)new_environ);
+		return (ERROR);
+	}
+	// environ が heap領域 に配置されている時はfreeする
+	if ((void *)environ < (void *)&new_environ)
+		free(environ);
+	environ = new_environ;
+	return (0);
+}
+
+/*
  * 標準ライブラリの setenv() と同じ動作をする
  */
 int	setenv(const char *name, const char *value, int rewrite)
 {
 	update_env(name, value, rewrite);  // TODO: update_env() が成功したら return (0) する
 	// 環境変数が存在していない場合, sizeof(environ) + 1 確保して追加. environの指す先のポインタを変更する
+	expand_and_add_env(name, value);
 	return (0);
 }
 
