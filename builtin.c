b700157070e738525f42537e780b86763ff5eb5e
Junichi Tanaka <junichi200123@gmail.com>
2021-05-01 13:23:23 +0900

    ビルトインコマンドの関数を取得する get_builtin_func() を定義

--- builtin.c
+++ builtin.c
@@ -1,9 +1,19 @@
-#include "minishell.h"
+#include "builtin.h"
 
-bool	is_buildin_command(char *cmd_name)
+bool	is_builtin_command(char *cmd_name)
 {
 	if (ft_strncmp(cmd_name, "echo", 5) == 0)
 		return (true);
 	return (false);
 }
 
+/*
+ * ビルトインコマンド(関数)のポインタを返す.
+ * これが上手くいけば is_builtin_command() はいらないかも
+ */
+t_builtin_cmd	*get_builtin_func(char *cmd_name)
+{
+	if (ft_strncmp(cmd_name, "echo", 5) == 0)
+		return (echo);
+	return (NULL);
+}
