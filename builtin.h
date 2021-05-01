464714a0d57f0b54e5b422c1115df0e00cb95450
Junichi Tanaka <junichi200123@gmail.com>
2021-05-01 13:33:45 +0900

    get_builtin_func() を builtin.h に追加

--- builtin.h
+++ builtin.h
@@ -10,7 +10,8 @@
 // ビルトインコマンドは argv を受け取って, ステータスを返す
 typedef int t_builtin_cmd(char**);
 
-bool	is_builtin_command(char *cmd_name);
-int		echo(char **argv);
+bool			is_builtin_command(char *cmd_name);
+t_builtin_cmd	*get_builtin_func(char *cmd_name);
+int				echo(char **argv);
 
 #endif
