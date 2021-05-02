21c506e57b585a91a8ff0c692f2592c92ebe10ec
Junichi Tanaka <junichi200123@gmail.com>
2021-05-03 03:16:03 +0900

    builtin_exit() を実装

--- builtin.c
+++ builtin.c
@@ -7,7 +7,8 @@ bool	is_builtin_command(char *cmd_name)
 		|| ft_strcmp(cmd_name, "pwd") == 0
 		|| ft_strcmp(cmd_name, "env") == 0
 		|| ft_strcmp(cmd_name, "export") == 0
-		|| ft_strcmp(cmd_name, "unset") == 0)
+		|| ft_strcmp(cmd_name, "unset") == 0
+		|| ft_strcmp(cmd_name, "exit") == 0)
 		return (true);
 	return (false);
 }
@@ -30,5 +31,7 @@ t_builtin_cmd	*get_builtin_func(char *cmd_name)
 		return (builtin_export);
 	if (ft_strcmp(cmd_name, "unset") == 0)
 		return (builtin_unset);
+	if (ft_strcmp(cmd_name, "exit") == 0)
+		return (builtin_exit);
 	return (NULL);
 }
