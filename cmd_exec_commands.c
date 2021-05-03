a4eb60d19457c182d4c4d088970f904580e1f540
Junichi Tanaka <junichi200123@gmail.com>
2021-05-03 12:43:09 +0900

    exec_commandsでビルトインコマンドを実行できるようにした

--- cmd_exec_commands.c
+++ cmd_exec_commands.c
@@ -1,5 +1,6 @@
 #include "env.h"
 #include "execution.h"
+#include "builtin.h"
 
 static int	cmd_connect_pipe(
 	int pipe_prev_fd[2], int pipe_fd[2], t_list **pid_lst, int pid)
@@ -10,6 +11,42 @@ static int	cmd_connect_pipe(
 	if (!cmd_lstadd_back_pid(pid_lst, pid))
 		return (ERROR);
 	return (0);
+}
+
+/* ビルトインコマンドを実行する
+ */
+int	cmd_exec_builtin(t_command_invocation *command,
+	int pipe_prev_fd[2], int pipe_fd[2])
+{
+	int	stdoutfd;
+	int	stdinfd;
+	int	status;
+
+	printf("Run builtin command: %s\n", command->exec_and_args[0]);
+	stdoutfd = dup(STDOUT_FILENO);
+	stdinfd = dup(STDIN_FILENO);
+	if (pipe_prev_fd[1] >= 0)
+	{
+		close(pipe_prev_fd[1]);
+		if (dup2(pipe_prev_fd[0], STDIN_FILENO) == -1)
+			put_err_msg_and_exit("error parent dup2()");
+		close(pipe_prev_fd[0]);
+	}
+	if (command->piped_command)
+	{
+		close(pipe_fd[0]);
+		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
+			put_err_msg_and_exit("error parent dup2()");
+		close(pipe_fd[1]);
+	}
+	if (cmd_set_input_file(command) == ERROR
+		|| cmd_set_output_file(command) == ERROR)
+		return (put_err_msg_and_ret("error input/output file"));
+	status = get_builtin_func((char *)command->exec_and_args[0])((char **)command->exec_and_args);
+	if (dup2(stdoutfd, STDOUT_FILENO) == -1
+		|| dup2(stdinfd, STDIN_FILENO) == -1)
+		put_err_msg_and_exit("error parent revert stdio back");
+	return (status);
 }
 
 /*
@@ -30,15 +67,19 @@ int	cmd_exec_commands(t_command_invocation *command)
 	{
 		if (pipe(pipe_fd) == -1)
 			return (put_err_msg_and_ret("error pipe()"));
-		// TODO: ビルドインコマンドだった場合はforkせずに親プロセスで実行
-		pid = fork();
-		if (pid < 0)
-			return (put_err_msg_and_ret("error fork()"));
-		else if (pid == 0)
-			cmd_exec_command(command, pipe_prev_fd, pipe_fd);
+		// ビルドインコマンドだった場合はforkせずに親プロセスで実行
+		if (is_builtin_command((char *)command->exec_and_args[0]))
+			cmd_exec_builtin(command, pipe_prev_fd, pipe_fd);
 		else
-			if (cmd_connect_pipe(pipe_prev_fd, pipe_fd, &pid_lst, pid) != 0)
-				return (put_err_msg_and_ret("error cmd_connect_pipe()"));
+		{
+			pid = fork();
+			if (pid < 0)
+				return (put_err_msg_and_ret("error fork()"));
+			else if (pid == 0)
+				cmd_exec_command(command, pipe_prev_fd, pipe_fd);
+		}
+		if (cmd_connect_pipe(pipe_prev_fd, pipe_fd, &pid_lst, pid) != 0)
+			return (put_err_msg_and_ret("error cmd_connect_pipe()"));
 		command = command->piped_command;
 	}
 	return (cmd_wait_pid_lst(pid_lst));
