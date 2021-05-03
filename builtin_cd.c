d11b0b91658494508af6e397a0704af171596262
Junichi Tanaka <junichi200123@gmail.com>
2021-05-04 01:19:35 +0900

    builtin_echo.c のNormエラー修正

--- builtin_cd.c
+++ builtin_cd.c
@@ -5,9 +5,9 @@ int	builtin_cd(char **argv)
 {
 	char	*dest_path;
 
-	if (ptrarr_len((void**) argv) > 2)
+	if (ptrarr_len((void **)argv) > 2)
 		return (put_err_msg_and_ret("too many arguments"));
-	if (ptrarr_len((void**) argv) == 2)
+	if (ptrarr_len((void **)argv) == 2)
 		dest_path = ft_strdup(argv[1]);
 	else
 		dest_path = get_env_val("HOME");
