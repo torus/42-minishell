0a744a03b64f85cc3b6164dbe3c16d57b0660ecd
Junichi Tanaka <junichi200123@gmail.com>
2021-05-03 18:57:40 +0900

    cmd_connect_pipe() から pid_lst に pid を追加する処理を削除

--- cmd_exec_commands.c
+++ cmd_exec_commands.c
@@ -3,13 +3,11 @@
 #include "builtin.h"
 
 static int	cmd_connect_pipe(
-	int pipe_prev_fd[2], int pipe_fd[2], t_list **pid_lst, int pid)
+	int pipe_prev_fd[2], int pipe_fd[2])
 {
 	if (pipe_prev_fd[1] != -1)
 		cmd_close_pipe(pipe_prev_fd);
 	cmd_copy_pipe(pipe_prev_fd, pipe_fd);
-	if (!cmd_lstadd_back_pid(pid_lst, pid))
-		return (ERROR);
 	return (0);
 }
 
@@ -76,8 +74,10 @@ int	cmd_exec_commands(t_command_invocation *command)
 				return (put_err_msg_and_ret("error fork()"));
 			else if (pid == 0)
 				cmd_exec_command(command, pipe_prev_fd, pipe_fd);
+			if (!cmd_lstadd_back_pid(&pid_lst, pid))
+				return (ERROR);
 		}
-		if (cmd_connect_pipe(pipe_prev_fd, pipe_fd, &pid_lst, pid) != 0)
+		if (cmd_connect_pipe(pipe_prev_fd, pipe_fd) != 0)
 			return (put_err_msg_and_ret("error cmd_connect_pipe()"));
 		command = command->piped_command;
 	}
