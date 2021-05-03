1f66a251a6b83be7b6c02c7ea30a5d3bab37999e
Junichi Tanaka <junichi200123@gmail.com>
2021-05-04 02:29:08 +0900

    builtin_cd のメモリリーク修正

--- builtin_cd.c
+++ builtin_cd.c
@@ -4,6 +4,7 @@
 int	builtin_cd(char **argv)
 {
 	char	*dest_path;
+	char	chdir_status;
 
 	if (ptrarr_len((void **)argv) > 2)
 		return (put_err_msg_and_ret("too many arguments"));
@@ -11,7 +12,9 @@ int	builtin_cd(char **argv)
 		dest_path = ft_strdup(argv[1]);
 	else
 		dest_path = get_env_val("HOME");
-	if (chdir(dest_path) < 0)
+	chdir_status = chdir(dest_path);
+	free(dest_path);
+	if (chdir_status < 0)
 		return (put_err_msg_and_ret("No such file or directory"));
 	return (0);
 }
