982a8c34f62bcd2c874e356d572f959db3c60319
Junichi Tanaka <junichi200123@gmail.com>
2021-05-03 21:53:32 +0900

    cmd_exec_builtin() で想定する入力をパイプではない単体のコマンドという風に変更

--- cmd_exec_commands.c
+++ cmd_exec_commands.c
@@ -12,45 +12,17 @@ static int	cmd_connect_pipe(
 }
 
 /* ビルトインコマンドを実行する
+ * パイプではなく単体で実行されることを想定している
  */
-int	cmd_exec_builtin(t_command_invocation *command,
-	int pipe_prev_fd[2], int pipe_fd[2])
+int	cmd_exec_builtin(t_command_invocation *command)
 {
-	int	stdoutfd;
-	int	stdinfd;
 	int	status;
 
-	stdoutfd = dup(STDOUT_FILENO);
-	stdinfd = dup(STDIN_FILENO);
-	if (pipe_prev_fd[1] >= 0)
-	{
-		close(pipe_prev_fd[1]);
-		if (dup2(pipe_prev_fd[0], STDIN_FILENO) == -1)
-			put_err_msg_and_exit("error parent dup2()");
-		close(pipe_prev_fd[0]);
-	}
-	if (command->piped_command)
-	{
-		close(pipe_fd[0]);
-		if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
-			put_err_msg_and_exit("error parent dup2()");
-		close(pipe_fd[1]);
-	}
 	if (cmd_set_input_file(command) == ERROR
 		|| cmd_set_output_file(command) == ERROR)
 		return (put_err_msg_and_ret("error parent input/output file"));
 	status = get_builtin_func((char *)command->exec_and_args[0])((char **)command->exec_and_args);
-	if (dup2(stdoutfd, STDOUT_FILENO) == -1
-		|| dup2(stdinfd, STDIN_FILENO) == -1)
-		put_err_msg_and_exit("error parent revert stdio back");
-	// 最後のコマンドだった場合, ステータスをセットする
-	/* TODO:
-	 * このままだと wait_pid_lst で並列で走らせていた子プロセスのステータスで上書きされるから,
-	 * t_command_invocation に pid を持たせるなど設計を変更し,
-	 * 最後のコマンドか否かを判定できるようにしたほうが良さそう
-	 */
-	if (!command->piped_command)
-		set_status(status);
+	set_status(status);
 	return (status);
 }
 
