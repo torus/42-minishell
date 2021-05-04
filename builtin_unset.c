2c78082b5c94cdd22f3a6d512f731e4442801027
Junichi Tanaka <junichi200123@gmail.com>
2021-05-04 20:10:19 +0900

    builtin_unset() で渡されたキーが正しくない時にエラーメッセージを表示する

--- builtin_unset.c
+++ builtin_unset.c
@@ -1,4 +1,41 @@
 #include "builtin.h"
+#include "minishell.h"
+
+/*
+ * unset に渡すキーとして正しいか?
+ * 環境変数のキーに設定出来ないやつは駄目という認識で良さそう
+ */
+bool	is_valid_key(char *key)
+{
+	int	i;
+
+	i = 0;
+	if (!(ft_isalpha(key[i]) || key[i] == '_'))
+		return (false);
+	while (key[i])
+	{
+		if (!(ft_isalnum(key[i]) || key[i] == '_'))
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
+static void	put_unset_errmsg(char *cmd_name, char *keyname)
+{
+	char	*tmp;
+	char	*errmsg;
+
+	tmp = ft_strjoin("`", keyname);
+	if (!tmp)
+		put_minish_err_msg_and_exit(1, cmd_name, "ft_strjoin() is failed");
+	errmsg = ft_strjoin(tmp, "': not a valid identifier");
+	free(tmp);
+	if (!errmsg)
+		put_minish_err_msg_and_exit(1, cmd_name, "ft_strjoin() is failed");
+	put_minish_err_msg(cmd_name, errmsg);
+	free(errmsg);
+}
 
 /*
  * builtin unset command
@@ -6,7 +43,25 @@
  */
 int	builtin_unset(char **argv)
 {
-	argv++;
+	int		i;
+	bool	is_all_key_ok;
+
+	i = 1;
+	is_all_key_ok = true;
+	// 全てのキーが正しいかどうかチェックする
+	while (argv[i])
+	{
+		if (!is_valid_key(argv[i]))
+		{
+			is_all_key_ok = false;
+			put_unset_errmsg(argv[0], argv[i]);
+		}
+		i++;
+	}
+	if (!is_all_key_ok)
+		return (1);
+
+	i = 1;
 	while (*argv)
 	{
 		ft_unsetenv(*argv);
