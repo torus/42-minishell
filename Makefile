b6ecca6bd2f4d6c2fe24b7ff3d4a2c1cabf4920e
Junichi Tanaka <junichi200123@gmail.com>
2021-05-05 10:27:27 +0900

    リダイレクション周りの関数を cmd_redirection.c に入れた

--- Makefile
+++ Makefile
@@ -7,13 +7,13 @@ LIBFT_MAKE = $(MAKE) -C $(LIBFT_PATH)
 LIBFT_LIB = ./libft/libft.a
 
 HEADER_FILES = minishell.h
-SRCS = cmd_cmd_invocation.c cmd_cmd_invocation2.c cmd_exec_command.c	\
-	cmd_exec_commands.c cmd_pipe.c convert_ast2cmdinvo.c				\
-	env.c exec.c lexer1.c lexer2.c minishell.c parse1.c parse2.c		\
-	parse_utils.c parse_utils2.c path.c string_node2string.c			\
-	expand_env_var.c expand_string_node.c split_expanded_str.c			\
-	cmd_status.c signal.c env_setter.c minishell_error_msg.c			\
-	builtin.c builtin_cd.c builtin_echo.c builtin_env.c builtin_exit.c	\
+SRCS = cmd_cmd_invocation.c cmd_cmd_invocation2.c cmd_exec_command.c		\
+	cmd_exec_commands.c cmd_pipe.c cmd_redirection.c convert_ast2cmdinvo.c	\
+	env.c exec.c lexer1.c lexer2.c minishell.c parse1.c parse2.c			\
+	parse_utils.c parse_utils2.c path.c string_node2string.c				\
+	expand_env_var.c expand_string_node.c split_expanded_str.c				\
+	cmd_status.c signal.c env_setter.c minishell_error_msg.c				\
+	builtin.c builtin_cd.c builtin_echo.c builtin_env.c builtin_exit.c		\
 	builtin_export.c builtin_pwd.c builtin_unset.c
 
 OBJS = $(SRCS:.c=.o)
---
+++ cmd_redirection.c
@@ -0,0 +1,105 @@
+#include "minishell.h"
+
+/*
+ * リダイレクション用に渡された文字列の環境変数展開を行う.
+ *
+ * 環境変数展開を行った結果, 文字列が0個や2つ以上の場合はエラーを表すNULLを返す.
+ * エラーの場合, エラーメッセージをstderrに出力する.
+ *
+ * red_target: リダイレクション先の文字列
+ *
+ * return: 環境変数展開などが完了した文字列.
+ *   展開した結果文字列が2つ以上の場合はエラーなのでNULLを返す.
+ */
+static char	*expand_redirect_filepath(char *red_target)
+{
+	char	*expanded_str;
+	char	**splitted_expanded_str;
+	char	*errmsg;
+	char	*filepath;
+
+	expanded_str = expand_env_var(red_target);
+	splitted_expanded_str = split_expanded_str(expanded_str);
+	free(expanded_str);
+	if (!splitted_expanded_str
+		|| ptrarr_len((void **)splitted_expanded_str) != 1)
+	{
+		errmsg = ft_strjoin(red_target, ": ambiguous redirect\n");
+		if (errmsg)
+			put_err_msg(errmsg);
+		free_ptrarr((void **)splitted_expanded_str);
+		free(errmsg);
+		return (NULL);
+	}
+	filepath = ft_strdup(splitted_expanded_str[0]);
+	free_ptrarr((void **)splitted_expanded_str);
+	return (filepath);
+}
+
+/*
+ * open input file as stdin if command->input_file_path is exist
+ *
+ * command: command.
+ *
+ * return: return -1 if error has occurred, otherwise, return 0.
+ */
+int	cmd_set_input_file(t_command_invocation *command)
+{
+	int					fd;
+	t_list				*current;
+	t_cmd_redirection	*red;
+	char				*filepath;
+
+	current = command->input_redirections;
+	while (current)
+	{
+		red = (t_cmd_redirection *)current->content;
+		filepath = expand_redirect_filepath((char *)red->filepath);
+		if (!filepath)
+			return (ERROR);
+		fd = open(filepath, O_RDONLY);
+		free(filepath);
+		if (fd == -1)
+			return (put_err_msg_and_ret("error input file open()"));
+		if (!current->next && dup2(fd, STDIN_FILENO) == -1)
+			return (put_err_msg_and_ret("error dup2(fd, STDIN_NO)"));
+		current = current->next;
+	}
+	return (0);
+}
+
+/*
+ * open output file as stdout if command->output_file_path is exist
+ *
+ * command: command.
+ *
+ * return: return -1 if error has occurred, otherwise, return 0.
+ */
+int	cmd_set_output_file(t_command_invocation *command)
+{
+	int					fd;
+	t_list				*current;
+	int					flag_open;
+	char				*filepath;
+
+	current = command->output_redirections;
+	while (current)
+	{
+		filepath = expand_redirect_filepath(
+				(char *)((t_cmd_redirection *)current->content)->filepath);
+		if (!filepath)
+			return (ERROR);
+		flag_open = O_TRUNC;
+		if (((t_cmd_redirection *)current->content)->is_append)
+			flag_open = O_APPEND;
+		fd = open(filepath, O_WRONLY | O_CREAT | flag_open,
+				S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+		free(filepath);
+		if (fd == -1)
+			return (put_err_msg_and_ret("error output file open()"));
+		if (!current->next && dup2(fd, STDOUT_FILENO) == -1)
+			return (put_err_msg_and_ret("error dup2(fd, STDOUT_NO)"));
+		current = current->next;
+	}
+	return (0);
+}
