1b446c3ff6f95d37aa2ef38856070df0b404bdac
Junichi Tanaka <junichi200123@gmail.com>
2021-05-05 23:30:56 +0900

    exitで引数が渡され無かった時は前の終了ステータスでexitする

--- builtin_exit.c
+++ builtin_exit.c
@@ -1,6 +1,34 @@
 #include "minishell.h"
 #include "builtin.h"
 
+static void	put_exit_errmsg_and_exit(char *exit_status)
+{
+	char	*tmp;
+
+	tmp = ft_strjoin(exit_status, ": numeric argument required");
+	if (!tmp)
+		put_minish_err_msg_and_exit(2, "exit", "ft_strjoin() failed");
+	put_minish_err_msg_and_exit(2, "exit", tmp);
+}
+
+static int	exit_atoi(char *str)
+{
+	int	num;
+	int	i;
+
+	i = 0;
+	num = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i])
+			|| num > (INT_MAX - (str[i] - '0')) / 10)
+			put_exit_errmsg_and_exit(str);
+		num = num * 10 + (str[i] - '0');
+		i++;
+	}
+	exit(num);
+}
+
 /*
  * builtin exit command
  * argv: ["exit", "0"]
@@ -13,8 +41,8 @@ int	builtin_exit(char **argv)
 	if (argv_len > 2)
 		return (put_minish_err_msg_and_ret(1, "exit", "too many arguments"));
 	else if (argv_len == 2)
-		exit(ft_atoi(argv[1]));
+		exit_atoi(argv[1]);
 	else
-		exit(0);
+		exit(get_status());
 	return (0);
 }
