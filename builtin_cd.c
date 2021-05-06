26ae1b1fe36c2509bfa7d234a994a1779a715360
Junichi Tanaka <junichi200123@gmail.com>
2021-05-07 04:03:29 +0900

    "cd -" と "cd 絶対パス" に対応した.

--- builtin_cd.c
+++ builtin_cd.c
@@ -1,28 +1,7 @@
 #include "builtin.h"
 #include "env.h"
+#include "path.h"
 #include "minishell.h"
-
-char	*g_the_current_directory = NULL;
-
-/* - $OLDPWD に現在の $PWD の値を保存
- * - $PWD に dest_path を保存
- * - g_the_current_directory に dest_path を保存
- */
-int	set_working_directory(char *dest_path)
-{
-}
-
-/* - This is `cd -', equivalent to `cd $OLDPWD'
- *   dest_path == "-" だった場合, $OLDPWD に移動して,
- *   $OLDPWD=$PWD にして, $PWDを更新する.
- *   $OLDPWDに移動失敗した場合は移動しないし, $OLDPWD, $PWD を更新しない.
- * - 絶対パス('/'から始まる)場合は $CDPATH から検索しない
- * - ディレクトリがシンボリックリンクだった場合, chdir(dest_path)して,
- *   the_current_directory=dest_path にする.
- */
-static int	change_directory(char *dest_path)
-{
-}
 
 static void	put_cd_errmsg(char *dest_path)
 {
@@ -39,6 +18,87 @@ static void	put_cd_errmsg(char *dest_path)
 		put_minish_err_msg_and_exit(1, "cd", "generating errmsg is failed!");
 	put_minish_err_msg("cd", errmsg);
 	free(errmsg);
+}
+
+/* 絶対パスを構築する
+ * "{g_cwd}/{relative_path}" を返す
+ */
+/*
+char *get_absolute_path(char *relative_path)
+{
+	return (NULL);
+}
+*/
+
+/* - ディレクトリがシンボリックリンクだった場合, chdir(dest_path)して,
+ *   the_current_directory=dest_path にする.
+ *
+ * 移動出来たら0, 移動出来なかったら-1を返す
+ */
+static int	change_to_directory(char *dest_path)
+{
+	char	*abs_path;
+	int		status;
+
+	// TODO: 絶対パスではない場合絶対パスを生成
+	abs_path = ft_strdup(dest_path);
+	status = chdir(abs_path);
+	if (status == 0)
+	{
+		set_current_working_directory(abs_path);
+		free(abs_path);
+		return (0);
+	}
+	free(abs_path);
+	return (-1);
+}
+
+/* - This is `cd -', equivalent to `cd $OLDPWD'
+ *   dest_path == "-" だった場合, $OLDPWD に移動して,
+ *   $OLDPWD=$PWD にして, $PWDを更新する.
+ *   $OLDPWDに移動失敗した場合は移動しないし, $OLDPWD, $PWD を更新しない.
+ * - 絶対パス('/'から始まる)の場合は $CDPATH から検索しない
+ * - $CDPATHから見つかった場合は移動先の絶対パスを標準出力する
+ */
+static int	change_directory(char *dest_path)
+{
+	char	*abs_path;
+	int		status;
+
+	printf("[debug]\t change_directory() start\n");
+	printf("[debug]\t dest_path = |%s|\n", dest_path);
+	// $OLDPWD に移動する場合
+	if (ft_strncmp(dest_path, "-", 2) == 0)
+	{
+		abs_path = get_env_val("OLDPWD");
+		printf("[debug]\t change to $OLDPWD start\n");
+		printf("[debug]\t $OLDPWD = |%s|\n", abs_path);
+		status = change_to_directory(abs_path);
+		printf("[debug]\t change_to_directory() = %d\n", status);
+		free(abs_path);
+		if (status == -1)
+			put_cd_errmsg(abs_path);
+		return (status);
+	}
+	if (dest_path[0] == '/')
+		return (change_to_directory(dest_path));
+	/*
+	// それ以外の場合は, 現在のディレクトリからdest_pathに移動出来ればOK.
+	// TODO: get_absolute_path(dest_path) で取得した絶対パスに chdir() する.
+	// TODO: 上記が失敗したら chdir(dest_path) を試す.
+	if (change_directory(dest_path) == 0)
+	{
+		return (0);
+	}
+	// 絶対パスが渡された場合 $CDPATH から検索しない
+	// そうでなければ $CDPATH を起点として移動してみる.
+	if (dest_path[0] != '/')
+	{
+
+	}
+	// TODO: $CDPATH から試していって, うまくいけばok. だめならreturn(1);
+	*/
+	return (1);
 }
 
 static int	cd_home(void)
@@ -71,7 +131,7 @@ int	builtin_cd(char **argv)
 		dest_path = ft_strdup(argv[1]);
 	else
 		return (cd_home());
-	chdir_status = chdir(dest_path);
+	chdir_status = change_directory(dest_path);
 	if (chdir_status < 0)
 		put_cd_errmsg(dest_path);
 	free(dest_path);
