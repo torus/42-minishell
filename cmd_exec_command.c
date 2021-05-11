732fa995ddba2671ab264fe43a60457013d4c89d
Junichi Tanaka <junichi200123@gmail.com>
2021-05-12 07:53:57 +0900

    cmd_execvp() は失敗したらexitするので呼び出し元でexitは必要ないので削除

--- cmd_exec_command.c
+++ cmd_exec_command.c
@@ -47,5 +47,4 @@ void	cmd_exec_command(t_command_invocation *command,
 		exit(builtin_func((char **)command->exec_and_args));
 	cmd_execvp((char *)command->exec_and_args[0],
 		(char **) command->exec_and_args);
-	exit(EXIT_FAILURE);
 }
