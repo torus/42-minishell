819f5f3a0c332693ec38a34c86e44914096c85c1
Junichi Tanaka <junichi200123@gmail.com>
2021-05-17 04:06:13 +0900

    コメントアウトしていた古いexpand_string_node()を削除

--- expand_string_node.c
+++ expand_string_node.c
@@ -102,22 +102,3 @@ char	**expand_string_node(t_parse_node_string *string_node)
 	}
 	return (result);
 }
-
-// /*
-//  * 一度文字列を全て展開した上でexpanded_strarrを生成する
-//  */
-// char	**expand_string_node(t_parse_node_string *string_node)
-// {
-// 	char	*restored_str;
-// 	char	*expanded_str;
-// 	char	**splitted_expanded_str;
-// 
-// 	restored_str = string_node2string(string_node);
-// 	printf("restored_str: |%s|\n", restored_str);
-// 	expanded_str = expand_env_var(restored_str);
-// 	free(restored_str);
-// 	printf("expanded_str: |%s|\n", expanded_str);
-// 	splitted_expanded_str = split_expanded_str(expanded_str);
-// 	free(expanded_str);
-// 	return (splitted_expanded_str);
-// }
