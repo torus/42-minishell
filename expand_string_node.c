6c5fc5df68a0a183f23a189c0b8e0ede6a96ebb6
Junichi Tanaka <junichi200123@gmail.com>
2021-05-17 09:45:51 +0900

    WIP: cmd_str をポインタ配列で扱う

--- expand_string_node.c
+++ expand_string_node.c
@@ -3,36 +3,29 @@
 typedef struct	s_cmd_str_node {
 	char					*text;
 	t_token_type			type;
-	struct s_cmd_str_node	*next;
 }				t_cmd_str_node;
 
-static t_cmd_str_node	*cmd_str_node_add_back(t_cmd_str_node **str_node, char *text, t_token_type tok_type)
+static bool	cmd_str_node_add_back(t_cmd_str_node ***str_node_arr, char *text, t_token_type tok_type)
 {
-	t_cmd_str_node	*current_node;
 	t_cmd_str_node	*new_node;
+	t_cmd_str_node	**tmp;
 
-	new_node = ft_calloc(1, sizeof(t_cmd_str_node));
+	new_node = malloc(sizeof(t_cmd_str_node));
 	if (!new_node)
-		return (NULL);
+		return (false);
 	new_node->text = text;
 	new_node->type = tok_type;
-	if (!*str_node)
-	{
-		*str_node = new_node;
-		return (new_node);
-	}
-	current_node = *str_node;
-	while (current_node->next)
-		current_node = current_node->next;
-	current_node->next = new_node;
-	return (new_node);
+	tmp = *str_node_arr;
+	*str_node_arr = (t_cmd_str_node **)ptrarr_add_ptr((void **)*str_node_arr, new_node);
+	free(tmp);
+	return (true);
 }
 
 /* ast_str_node を cmd_str_node に変換する(環境変数は展開する).
  */
-static t_cmd_str_node	*ast_str2cmd_str(t_parse_node_string *str_node)
+static t_cmd_str_node	**ast_str2cmd_str(t_parse_node_string *str_node)
 {
-	t_cmd_str_node	*result;
+	t_cmd_str_node	**result;
 	char			*expanded_str;
 
 	result = NULL;
@@ -43,44 +36,32 @@ static t_cmd_str_node	*ast_str2cmd_str(t_parse_node_string *str_node)
 		else
 			expanded_str = expand_env_var(str_node->text);
 		if (!cmd_str_node_add_back(&result, expanded_str, str_node->type))
-				return (NULL);
+			return (NULL);
 		if (str_node->next)
 			str_node = str_node->next->content.string;
 		else
 			str_node = NULL;
 	}
-	return (result);
+	return ((t_cmd_str_node **)result);
 }
 
 /* 中間表現構造体を文字列配列にする. */
-static char	**cmd_str2str_arr(t_cmd_str_node *str_node)
+static char	**cmd_str2str_arr(t_cmd_str_node **str_node)
 {
 	char	**result;
-	int		last_idx;
+	int		i;
 
-	last_idx = -1;  // resultの最後の文字列への添字
+	i = 0;
 	result = NULL;
-	while (str_node)
+	while (str_node[i])
 	{
 		// 前の文字列の右端と今回の文字列左端に空白が無ければくっつける
-		if ((str_node->type == TOKTYPE_EXPANDABLE_QUOTED
-				|| str_node->type == TOKTYPE_NON_EXPANDABLE)
-			&& (last_idx >= 0
-				&& result[last_idx][ft_strlen(result[last_idx]) - 1] != ' '
-				&& str_node->text[0] != ' '))
+		int j = 0;
+		while (str_node[i]->text[j])
 		{
-			char	*tmp = result[last_idx];
-			result[last_idx] = ft_strjoin(tmp, str_node->text);
-			free(tmp);
+			
 		}
-		else
-		{
-			char	**tmp = result;
-			result = (char **)ptrarr_add_ptr((void **)result, ft_strdup(str_node->text));
-			free(tmp);
-			last_idx = ptrarr_len((void **)result) - 1;
-		}
-		str_node = str_node->next;
+		i++;
 	}
 	return (result);
 }
@@ -88,34 +69,32 @@ static char	**cmd_str2str_arr(t_cmd_str_node *str_node)
 char	**expand_string_node(t_parse_node_string *string_node)
 {
 	// string_nodeを中間表現構造体(テキストとタイプを持つ)の配列に変換する.
-	t_cmd_str_node *cmd_str = ast_str2cmd_str(string_node);
+	t_cmd_str_node **cmd_str = ast_str2cmd_str(string_node);
 
-	t_cmd_str_node *tmp = cmd_str;
 	int i = 0;
-	while (tmp)
+	while (cmd_str[i])
 	{
 		printf("%d:\n", i);
-		printf("\t|%s|\n", tmp->text);
-		if (tmp->type == TOKTYPE_EXPANDABLE)
+		printf("\t|%s|\n", cmd_str[i]->text);
+		if (cmd_str[i]->type == TOKTYPE_EXPANDABLE)
 			printf("\tTOKTYPE_EXPANDABLE\n");
-		else if (tmp->type == TOKTYPE_EXPANDABLE_QUOTED)
+		else if (cmd_str[i]->type == TOKTYPE_EXPANDABLE_QUOTED)
 			printf("\tTOKTYPE_EXPANDABLE_QUOTED\n");
-		else if (tmp->type == TOKTYPE_NON_EXPANDABLE)
+		else if (cmd_str[i]->type == TOKTYPE_NON_EXPANDABLE)
 			printf("\tTOKTYPE_NON_EXPANDABLE\n");
 		else
-			printf("\tUNKNOWN: %d\n", tmp->type);
+			printf("\tUNKNOWN: %d\n", cmd_str[i]->type);
 		i++;
-		tmp = tmp->next;
 	}
 	// 中間表現構造体を文字列配列にする.
 	char	**result = cmd_str2str_arr(cmd_str);
 
-	while (cmd_str)
+	i = 0;
+	while (cmd_str[i])
 	{
-		free(cmd_str->text);
-		t_cmd_str_node *tmp = cmd_str;
-		cmd_str = cmd_str->next;
-		free(tmp);
+		free(cmd_str[i]->text);
+		free(cmd_str[i]);
+		i++;
 	}
 	return (result);
 }
