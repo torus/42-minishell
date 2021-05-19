23b839fd1379eadde1033972ac62b4a74f80194d
Junichi Tanaka <junichi200123@gmail.com>
2021-05-19 19:02:06 +0900

    $PWDと$SHLVLをシェル起動時に処理する

---
+++ shell_initialization.c
@@ -0,0 +1,53 @@
+#include <unistd.h>
+#include "minishell.h"
+#include "env.h"
+#include "libft/libft.h"
+
+static void	init_pwd(void)
+{
+	char	*pwd;
+	char	*tmp;
+
+	pwd = get_env_val("PWD");
+	if (!pwd)
+	{
+		tmp = getcwd(NULL, 0);
+		ft_setenv("PWD", tmp, 1);
+		free(tmp);
+	}
+	free(pwd);
+}
+
+static void	init_shlvl(void)
+{
+	char	*shlvl;
+	char	*num_str;
+	int		num;
+
+	shlvl = get_env_val("SHLVL");
+	if (shlvl)
+	{
+		num = ft_atoi(shlvl);
+		if (num < 0)
+			num = 0;
+		else
+			num++;
+	}
+	else
+		num = 1;
+	num_str = ft_itoa(num);
+	ft_setenv("SHLVL", num_str, 1);
+	free(num_str);
+	free(shlvl);
+}
+
+/* シェルの環境を初期化する */
+int	initialize_shell(void)
+{
+	set_shell_sighandlers();
+	// PWD
+	init_pwd();
+	// SHLVL
+	init_shlvl();
+	return (0);
+}
