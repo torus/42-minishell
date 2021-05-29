14c3cc63a96b6e86ac027ceca08e449c839eac1c
Junichi Tanaka <junichi200123@gmail.com>
2021-05-29 22:17:51 +0900

    ダブルクオート内のエスケープの挙動を修正

--- lexer3.c
+++ lexer3.c
@@ -27,6 +27,7 @@ int	lex_check_redirection_with_fd(t_parse_buffer *buf, t_token *result)
 	return (1);
 }
 
+#include <stdio.h>
 int	lex_escaped(t_parse_buffer *buf, t_token *result)
 {
 	char	ch;
@@ -35,7 +36,18 @@ int	lex_escaped(t_parse_buffer *buf, t_token *result)
 	if (ch == '\\')
 	{
 		ch = lex_getc(buf);
-		result->text[0] = ch;
+		// printf("escaped_char: %c\n", ch);
+		if (buf->lex_stat == LEXSTAT_NORMAL
+			|| (buf->lex_stat == LEXSTAT_DOUBLE_QUOTED
+				&& (ch == '$' || ch == '\"' || ch == '\'' || ch == '\\' || ch == '`')))
+		{
+			result->text[0] = ch;
+			result->length = 1;
+			result->type = TOKTYPE_NON_EXPANDABLE;
+			return (1);
+		}
+		lex_ungetc(buf);
+		result->text[0] = '\\';
 		result->length = 1;
 		result->type = TOKTYPE_NON_EXPANDABLE;
 		return (1);
