3132aaf31897870ff40d64bf3a3c0b614d4f5d72
Junichi Tanaka <junichi200123@gmail.com>
2021-05-29 16:12:40 +0900

    builtin_export Normエラー修正

--- builtin_export2.c
+++ builtin_export2.c
@@ -8,41 +8,27 @@
 #include "minishell.h"
 #include "utils.h"
 
-
-static char    *escape_string(char *str)
+static void	print_escaped_value(char *str)
 {
-	char	*result;
-	char	*tmp;
-	int		i;
-	
+	int	i;
+
 	i = 0;
-	result = ft_strdup("");
 	while (str[i])
 	{
 		if (str[i] == '"' || str[i] == '$' || str[i] == '`' || str[i] == '\\')
 		{
-			tmp = ft_substr(str, 0, i);
-			result = strjoin_and_free_both(result, tmp);
-			result = strjoin_and_free_first(result, "\\");
-			tmp = ft_substr(str + i, 0, 1);
-			result = strjoin_and_free_both(result, tmp);
-			str += i + 1;
-			i = 0;
+			ft_putchar_fd('\\', STDOUT_FILENO);
+			ft_putchar_fd(str[i], STDOUT_FILENO);
 		}
+		else
+			ft_putchar_fd(str[i], STDOUT_FILENO);
 		i++;
 	}
-	if (i)
-	{
-		tmp = ft_substr(str, 0, i);
-		result = strjoin_and_free_both(result, tmp);
-	}
-	return (result);
 }
 
 int	print_envs_with_declaration(void)
 {
 	t_var		*tmp_var;
-	char		*tmp;
 
 	tmp_var = g_shell.vars;
 	while (tmp_var)
@@ -55,9 +41,7 @@ int	print_envs_with_declaration(void)
 			{
 				write(STDOUT_FILENO, "=", 1);
 				write(STDOUT_FILENO, "\"", 1);
-				tmp = escape_string((char *)tmp_var->value);
-				ft_putstr_fd(tmp, STDOUT_FILENO);
-				free(tmp);
+				print_escaped_value((char *)tmp_var->value);
 				write(STDOUT_FILENO, "\"", 1);
 			}
 			write(STDOUT_FILENO, "\n", 1);
