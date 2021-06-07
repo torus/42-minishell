104e0bc06ca0c98958669ad13ece7c8438123c97
Toru Hisai <toru@torus.jp>
2021-06-07 10:42:10 +0900

    比較テストの見た目の調整。

--- test/test.h
+++ test/test.h
@@ -6,12 +6,23 @@
 #include <stdbool.h>
 
 #define CHECK(val) test_check((int64_t)val, #val)
-#define CHECK_EQ(actual, expected) test_check(actual == expected, #actual " == " #expected)
-#define CHECK_EQ_STR(actual, expected) \
-  do { \
-	if (!test_check(strcmp(actual, expected) == 0, #actual " == " #expected)) \
-		printf("    |%s| == |%s|\n", actual, expected); \
-  } while(0)
+#define CHECK_EQ(actual, expected)								\
+	do {														\
+		if (!test_check(										\
+				actual == expected,								\
+				#actual " == " #expected))						\
+			printf("      actual: %lld\n    expected: %lld\n",	\
+				   (long long)actual, (long long)expected);		\
+	} while(0)
+
+#define CHECK_EQ_STR(actual, expected)						\
+	do {													\
+		if (!test_check(									\
+				strcmp(actual, expected) == 0,				\
+				#actual " == " #expected))					\
+			printf("    |%s| == |%s|\n", actual, expected);	\
+	} while(0)
+
 #define TEST_CHAPTER(message) printf("#\n# " message "\n#\n")
 #define TEST_SECTION(message) printf("- " message "\n")
 
