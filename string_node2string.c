a98090027d60eaaf9aae4685dafe5a571023782c
Junichi Tanaka <junichi200123@gmail.com>
2021-06-22 02:15:02 +0900

    string_node2string() でmalloc()チェックをしっかり行う

--- string_node2string.c
+++ string_node2string.c
@@ -1,6 +1,12 @@
 #include "minishell.h"
 #include "utils.h"
 
+static void	string_node2string_malloc_check(char *result)
+{
+	if (!result)
+		put_minish_err_msg_and_exit(1, "strnode2str", "malloc() failed");
+}
+
 /*
  * Convert AST string node to string.
  */
@@ -9,17 +15,21 @@ char	*string_node2string(t_parse_node_string *string_node, bool is_add_quotes)
 	char	*result;
 
 	result = ft_strdup("");
+	string_node2string_malloc_check(result);
 	while (string_node)
 	{
 		if (is_add_quotes && string_node->type == TOKTYPE_NON_EXPANDABLE)
 			result = strjoin_and_free_first(result, "\'");
 		else if (is_add_quotes && string_node->type == TOKTYPE_EXPANDABLE_QUOTED)
 			result = strjoin_and_free_first(result, "\"");
+		string_node2string_malloc_check(result);
 		result = strjoin_and_free_first(result, string_node->text);
+		string_node2string_malloc_check(result);
 		if (is_add_quotes && string_node->type == TOKTYPE_NON_EXPANDABLE)
 			result = strjoin_and_free_first(result, "\'");
 		else if (is_add_quotes && string_node->type == TOKTYPE_EXPANDABLE_QUOTED)
 			result = strjoin_and_free_first(result, "\"");
+		string_node2string_malloc_check(result);
 		if (string_node->next)
 			string_node = string_node->next->content.string;
 		else
