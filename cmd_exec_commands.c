53dff237ad1833c811b2f2476ad149b4fa2ed2bd
Junichi Tanaka <junichi200123@gmail.com>
2021-06-22 04:12:42 +0900

    cmd_exec_commands() 内の各コマンドに対する処理を関数に切り分けた

--- cmd_exec_commands.c
+++ cmd_exec_commands.c
@@ -59,6 +59,27 @@ int	write_heredoc(t_command_invocation *command, int pipe_heredoc_fd[2])
 	return (0);
 }
 
+static int	cmd_exec_one_command(t_command_invocation *current_cmd,
+	int	pipe_fd[2], int pipe_prev_fd[2], int pipe_heredoc_fd[2])
+{
+	pid_t	pid;
+
+	if (pipe(pipe_fd) || cmd_set_heredoc_pipe_fd(current_cmd, pipe_heredoc_fd))
+		return (put_err_msg_and_ret("error pipe()"));
+	pid = fork();
+	if (pid < 0)
+		return (put_err_msg_and_ret("error fork()"));
+	else if (pid == 0)
+		cmd_exec_command(current_cmd, pipe_prev_fd, pipe_fd,
+			pipe_heredoc_fd);
+	write_heredoc(current_cmd, pipe_heredoc_fd);
+	cmd_close_pipe(pipe_heredoc_fd);
+	current_cmd->pid = pid;
+	if (cmd_connect_pipe(pipe_prev_fd, pipe_fd) != 0)
+		return (put_err_msg_and_ret("error cmd_connect_pipe()"));
+	return (0);
+}
+
 /*
  * fork and execute commands.
  *
@@ -68,7 +89,6 @@ int	write_heredoc(t_command_invocation *command, int pipe_heredoc_fd[2])
  */
 int	cmd_exec_commands(t_command_invocation *command)
 {
-	pid_t					pid;
 	int						pipe_fd[2];
 	int						pipe_prev_fd[2];
 	int						pipe_heredoc_fd[2];
@@ -81,17 +101,9 @@ int	cmd_exec_commands(t_command_invocation *command)
 	cmd_init_pipe_fd(pipe_prev_fd, STDIN_FILENO, -1);
 	while (current_cmd)
 	{
-		if (pipe(pipe_fd) || cmd_set_heredoc_pipe_fd(command, pipe_heredoc_fd))
-			return (put_err_msg_and_ret("error pipe()"));
-		pid = fork();
-		if (pid < 0) return (put_err_msg_and_ret("error fork()"));
-		else if (pid == 0)
-			cmd_exec_command(current_cmd, pipe_prev_fd, pipe_fd, pipe_heredoc_fd);
-		write_heredoc(current_cmd, pipe_heredoc_fd);
-		cmd_close_pipe(pipe_heredoc_fd);
-		current_cmd->pid = pid;
-		if (cmd_connect_pipe(pipe_prev_fd, pipe_fd) != 0)
-			return (put_err_msg_and_ret("error cmd_connect_pipe()"));
+		if (cmd_exec_one_command(current_cmd, pipe_fd, pipe_prev_fd,
+				pipe_heredoc_fd))
+			break ;
 		current_cmd = current_cmd->piped_command;
 	}
 	return (cmd_wait_commands(command));
