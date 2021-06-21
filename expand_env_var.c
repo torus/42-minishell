ec03685a4f36f2f7c04ce0279e1963c44ab33e3f
Junichi Tanaka <junichi200123@gmail.com>
2021-06-21 17:32:46 +0900

    必要なくなったTODOコメントを削除

--- expand_env_var.c
+++ expand_env_var.c
@@ -119,7 +119,6 @@ static bool	join_str_or_env(char **result,
  *   in($ABC="hoge"):       |'$''$'"ABC"'\'"$ABC""$ABC"|
  *   out:                   |'$''$'"ABC"'\'"hoge""hoge"|
  */
-// TODO: シングルクオートなどの処理を消す
 char	*expand_env_var(char *str)
 {
 	int		len;
