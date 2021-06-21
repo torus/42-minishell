cf40909e956f59383f3833bbf377c2d79c4b9bbe
Junichi Tanaka <junichi200123@gmail.com>
2021-06-22 03:11:48 +0900

    heredoc_expander.c のNormエラー修正

--- heredoc_expander.c
+++ heredoc_expander.c
@@ -4,18 +4,16 @@
 #include "execution.h"
 #include "utils.h"
 
-static void process_escape(char **result, char **str, int *len)
+static void	process_escape(char **result, char **str, int *len)
 {
 	char	*tmpstr;
 
-	// ここまでの文字列を繋げる
-	// バックスラッシュの次の文字が特殊文字('$' | '\')じゃなければバックスラッシュも入れる
-	tmpstr = ft_substr(*str, 0, *len + !((*str)[*len + 1] == '$' || (*str)[*len + 1] == '\\'));
+	tmpstr = ft_substr(*str, 0,
+			*len + !((*str)[*len + 1] == '$' || (*str)[*len + 1] == '\\'));
 	check_malloc_has_succeeded("heredoc expander", tmpstr);
 	*result = strjoin_and_free_both(*result, tmpstr);
 	check_malloc_has_succeeded("heredoc expander", *result);
 	*str += *len + 1;
-	// バックスラッシュの次の文字を繋げる
 	tmpstr = ft_substr(*str, 0, 1);
 	check_malloc_has_succeeded("heredoc expander", tmpstr);
 	*result = strjoin_and_free_both(*result, tmpstr);
@@ -33,8 +31,8 @@ static void process_escape(char **result, char **str, int *len)
 char	*expand_heredoc_document(char *str)
 {
 	char	*result;
-	int		len;  // 現在見ている範囲の長さ. 先頭は*strを直接動かす
-	bool	is_in_env;  // 現在環境変数にいるか
+	int		len;
+	bool	is_in_env;
 	bool	is_continue;
 
 	result = ft_strdup("");
@@ -44,14 +42,13 @@ char	*expand_heredoc_document(char *str)
 	is_in_env = false;
 	while (is_continue)
 	{
-		// エスケープの処理
 		if (str[len] == '\\')
 		{
 			if (is_in_env)
-				is_continue = exp_join_str_or_env(&result, &str, &len, &is_in_env);
+				is_continue = exp_join_str_or_env(&result, &str, &len,
+						&is_in_env);
 			process_escape(&result, &str, &len);
 		}
-		// ドルマーク($) 環境変数開始の処理
 		else if (exp_will_toggle_env(is_in_env, false, str, len))
 			is_continue = exp_join_str_or_env(&result, &str, &len, &is_in_env);
 		else
