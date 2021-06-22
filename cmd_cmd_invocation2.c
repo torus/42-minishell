ad7e9f28f7e7c7fe78d473c0bd79960898319c83
Junichi Tanaka <junichi200123@gmail.com>
2021-06-22 19:22:15 +0900

    readline4heredoc() のメモリリークを修正

--- cmd_cmd_invocation2.c
+++ cmd_cmd_invocation2.c
@@ -62,11 +62,10 @@ static void	readline4heredoc(t_cmd_redirection *red, const char *limit_str)
 		check_malloc_has_succeeded("heredoc", (void *)red->filepath);
 	}
 	rl_event_hook = NULL;
-	if (!input_str)
-	{
-		write(1, "\n", 1);
+	if (input_str)
 		free(input_str);
-	}
+	else
+		write(1, "\n", 1);
 }
 
 int	cmd_add_heredoc(t_command_invocation *command,
