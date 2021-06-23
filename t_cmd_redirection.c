d1590cb4ff83dda44c4a67ee56ae78cb26afde17
Junichi Tanaka <junichi200123@gmail.com>
2021-06-24 03:19:20 +0900

    cmd_redirection_add_back() を修正

--- t_cmd_redirection.c
+++ t_cmd_redirection.c
@@ -2,14 +2,17 @@
 
 t_cmd_redirection	*cmd_redirection_add_back(t_cmd_redirection **reds, t_cmd_redirection *new_red)
 {
+	t_cmd_redirection	*red;
+
 	new_red->next = NULL;
 	if (!*reds)
 	{
 		*reds = new_red;
 		return (new_red);
 	}
-	while ((*reds)->next)
-		(*reds) = (*reds)->next;
-	(*reds)->next = new_red;
+	red = *reds;
+	while (red->next)
+		red = red->next;
+	red->next = new_red;
 	return (new_red);
 }
