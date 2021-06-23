911c7aeb5cf0fa3a3bcdd364f6c1e7701f6916ba
Junichi Tanaka <junichi200123@gmail.com>
2021-06-24 05:34:06 +0900

    t_fd_reds_list->reds はt_cmd_redirectionをディープコピーしたものを入れる

--- fd_red_list_utils.c
+++ fd_red_list_utils.c
@@ -15,35 +15,44 @@ static t_in_fd_reds_list	*get_fd_red_from_list(t_in_fd_reds_list *fd_red_list, i
 
 static void	addback_new_fd_red2fd_red_list(t_in_fd_reds_list **fd_red_list, t_cmd_redirection *red)
 {
+	t_in_fd_reds_list	*tmp;
 	t_in_fd_reds_list	*new_fd_red;
 
 	new_fd_red = malloc(sizeof(t_in_fd_reds_list));
 	check_malloc_has_succeeded("fd_red_list", new_fd_red);
 	new_fd_red->fd = red->fd;
-	new_fd_red->reds = red;
 	cmd_init_pipe_fd(new_fd_red->heredoc_pipe, -1, -1);
+	red->next = NULL;
+	new_fd_red->reds = NULL;
+	cmd_redirection_add_back(&new_fd_red->reds, red);
 	new_fd_red->next = NULL;
 	if (!*fd_red_list)
 		*fd_red_list = new_fd_red;
 	else
 	{
-		while ((*fd_red_list)->next)
-			*fd_red_list = (*fd_red_list)->next;
-		(*fd_red_list)->next = new_fd_red;
+		tmp = *fd_red_list;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new_fd_red;
 	}
 }
 
 t_in_fd_reds_list	*reds2in_fd_reds_list(t_cmd_redirection *reds)
 {
 	t_in_fd_reds_list		*fd_red_list;
+	t_in_fd_reds_list		*fd_red_element;
+	t_cmd_redirection		*new_red;
 
 	fd_red_list = NULL;
 	while (reds)
 	{
-		if (!get_fd_red_from_list(fd_red_list, reds->fd))
-			addback_new_fd_red2fd_red_list(&fd_red_list, reds);
+		fd_red_element = get_fd_red_from_list(fd_red_list, reds->fd);
+		new_red = copy_t_cmd_redirection(reds);
+		check_malloc_has_succeeded("addback_new_fd_red2fd_red_list", new_red);
+		if (!fd_red_element)
+			addback_new_fd_red2fd_red_list(&fd_red_list, new_red);
 		else
-			cmd_redirection_add_back(&get_fd_red_from_list(fd_red_list, reds->fd)->reds, reds);
+			cmd_redirection_add_back(&fd_red_element->reds, new_red);
 		reds = reds->next;
 	}
 	return (fd_red_list);
--- t_cmd_redirection.c
+++ t_cmd_redirection.c
@@ -5,14 +5,31 @@ t_cmd_redirection	*cmd_redirection_add_back(t_cmd_redirection **reds, t_cmd_redi
 	t_cmd_redirection	*red;
 
 	new_red->next = NULL;
-	if (!*reds)
-	{
-		*reds = new_red;
-		return (new_red);
-	}
 	red = *reds;
-	while (red->next)
-		red = red->next;
-	red->next = new_red;
+	if (!red)
+		*reds = new_red;
+	else
+	{
+		while (red->next)
+			red = red->next;
+		red->next = new_red;
+	}
 	return (new_red);
+}
+
+t_cmd_redirection	*copy_t_cmd_redirection(t_cmd_redirection *original)
+{
+	t_cmd_redirection	*new;
+
+	new = malloc(sizeof(t_cmd_redirection));
+	if (!new)
+		return (NULL);
+	new->fd = original->fd;
+	new->filepath = ft_strdup(original->filepath);
+	if (!new->filepath)
+		return (NULL);
+	new->is_append = original->is_append;
+	new->is_heredoc = original->is_heredoc;
+	new->next = NULL;
+	return (new);
 }
