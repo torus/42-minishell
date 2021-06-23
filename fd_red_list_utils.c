d652c9b0585a78ee5bf077a099c12c250ce13acb
Junichi Tanaka <junichi200123@gmail.com>
2021-06-24 02:47:22 +0900

    addback_new_fd_red2fd_red_list() を修正

--- fd_red_list_utils.c
+++ fd_red_list_utils.c
@@ -13,19 +13,24 @@ static t_fd_red_list	*get_fd_red_from_list(t_fd_red_list *fd_red_list, int fd)
 	return (NULL);
 }
 
-static void	addback_new_fd_red2fd_red_list(t_fd_red_list *fd_red_list, t_cmd_redirection *red)
+static void	addback_new_fd_red2fd_red_list(t_fd_red_list **fd_red_list, t_cmd_redirection *red)
 {
-	t_fd_red_list	*new;
+	t_fd_red_list	*new_fd_red;
 
-	new = malloc(sizeof(t_cmd_redirection));
-	check_malloc_has_succeeded("fd_red_list", new);
-	new->fd = red->fd;
-	new->reds = red;
-	cmd_init_pipe_fd(new->pipe, -1, -1);
-	new->next = NULL;
-	while (fd_red_list->next)
-		fd_red_list = fd_red_list->next;
-	fd_red_list->next = new;
+	new_fd_red = malloc(sizeof(t_fd_red_list));
+	check_malloc_has_succeeded("fd_red_list", new_fd_red);
+	new_fd_red->fd = red->fd;
+	new_fd_red->reds = red;
+	cmd_init_pipe_fd(new_fd_red->pipe, -1, -1);
+	new_fd_red->next = NULL;
+	if (!*fd_red_list)
+		*fd_red_list = new_fd_red;
+	else
+	{
+		while ((*fd_red_list)->next)
+			*fd_red_list = (*fd_red_list)->next;
+		(*fd_red_list)->next = new_fd_red;
+	}
 }
 
 t_fd_red_list	*reds2fd_red_list(t_cmd_redirection *reds)
@@ -36,7 +41,7 @@ t_fd_red_list	*reds2fd_red_list(t_cmd_redirection *reds)
 	while (reds)
 	{
 		if (!get_fd_red_from_list(fd_red_list, reds->fd))
-			addback_new_fd_red2fd_red_list(fd_red_list, reds);
+			addback_new_fd_red2fd_red_list(&fd_red_list, reds);
 		else
 			cmd_redirection_add_back(&get_fd_red_from_list(fd_red_list, reds->fd)->reds, reds);
 		reds = reds->next;
